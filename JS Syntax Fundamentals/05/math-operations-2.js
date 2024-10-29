232d239eb3170bc24c1b96dbcac20723b879a539
Zlatin Rusev <zlatinrusev@gmail.com>
2024-10-29 15:08:52 +0100

    Solution

---
+++ JS Syntax Fundamentals/05/math-operations-2.js
@@ -0,0 +1,19 @@
+function solve(a, b, operator) {
+    let result
+    switch (operator) {
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': result = a / b; break;
+        case '%': result = a % b; break;
+        case '**': result = a ** b; break;
+    }
+    console.log(result)
+}
+
+solve(5, 5, '+')
+solve(5, 5, '-')
+solve(5, 5, '*')
+solve(5, 5, '/')
+solve(5, 5, '%')
+solve(5, 5, '**')
