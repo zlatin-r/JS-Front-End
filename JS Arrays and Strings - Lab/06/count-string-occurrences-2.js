1bd9aade06320e60ac4c00a7ef960fa7087c1347
Zlatin Rusev <zlatinrusev@gmail.com>
2024-10-31 12:58:45 +0100

    Solution

---
+++ JS Arrays and Strings - Lab/06/count-string-occurrences-2.js
@@ -0,0 +1,16 @@
+function solve(text, word) {
+    let words = text.split(' ');
+    let count = 0;
+
+    for (let w of words) {
+        if (w === word) {
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+solve('This is a word and it also is a sentence',
+    'is')
+solve('softuni is great place for learning new programming languages',
+    'softuni')
