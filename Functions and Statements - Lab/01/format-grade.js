fe0f9e5c8e82bab82a5b833a5102fffd711ef37d
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-01 14:57:38 +0100

    Solution

---
+++ Functions and Statements - Lab/01/format-grade.js
@@ -0,0 +1,17 @@
+function grade(score) {
+    if (score < 3) {
+        return 'Fail (2)'
+    } else if (score >= 3 && score < 3.5) {
+        return `Poor (${score.toFixed(2)})`
+    } else if (score >= 3.5 && score < 4.5) {
+        return `Good (${score.toFixed(2)})`
+    } else if (score >= 4.5 && score < 5.5) {
+        return `Very Good (${score.toFixed(2)})`
+    } else if (score >= 5.5) {
+        return 'Excellent'
+    }
+}
+
+console.log(grade(3.33))
+console.log(grade(4.50))
+console.log(grade(2.99))
