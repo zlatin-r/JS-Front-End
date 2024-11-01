6785d39f2ba6d9d52f937f68498195ded6f8bd9c
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-01 11:39:36 +0100

    Solution

--- JS Arrays & Strings - Exercise/Exercise/07/string-substring-2.js
+++ JS Arrays & Strings - Exercise/Exercise/07/string-substring-2.js
@@ -1,5 +1,16 @@
 function solve(word, text) {
-    if (text.toLowerCase().includes(word)) {
+
+    let lowerCaseWord = word.toLowerCase();
+    let lowerCaseText = text.toLowerCase().split(' ');
+    let found = false;
+
+    for (w of lowerCaseText) {
+        if (lowerCaseWord === w) {
+            found = true;
+            break;
+        }
+    }
+    if (found) {
         console.log(word);
     } else {
         console.log(`${word} not found!`);
