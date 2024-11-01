4338903d728d02c14c11e948bad53bbe76624ac1
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-01 15:05:14 +0100

    Solution

--- Functions and Statements - Lab/03/repeat-string-2.js
+++ Functions and Statements - Lab/03/repeat-string-2.js
@@ -1,10 +1,5 @@
 function repeatString(str, count) {
-    let result = '';
-
-    for (let i = 0; i < count; i++) {
-        result += str
-    }
-    return result;
+    return str.repeat(count)
 }
 
 console.log(repeatString('abc', 3))
