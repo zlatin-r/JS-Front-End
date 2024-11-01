5f15eaf394545a48e35458cdbb0d43572e434c95
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-01 12:11:33 +0100

    Solution

--- JS Arrays & Strings - Exercise/More Exercises/01/login.js
+++ JS Arrays & Strings - Exercise/More Exercises/01/login.js
@@ -4,19 +4,19 @@ function solve(arr) {
 
     for (let i = 0; i < 4; i++) {
         let input = arr[i + 1];
-        if (input === password) {
-            console.log(`User ${username} is logged in.`);
-            break;
 
+        if (input === password) {
+            console.log(`User ${username} logged in.`);
+            break;
+        } else if (i === 3) {
+            console.log(`User ${username} blocked!`);
+            break;
         } else {
-            console.log('Incorrect password. Try again');
-        }
-        if (i === 3) {
-            console.log(`User ${username} is blocked!`);
+            console.log('Incorrect password. Try again.');
         }
     }
 }
 
-solve(['Acer','login','go','let me in','recA'])
-solve(['momo','omom'])
-solve(['sunny','rainy','cloudy','sunny','not sunny'])
+solve(['Acer', 'login', 'go', 'let me in', 'recA'])
+solve(['momo', 'omom'])
+solve(['sunny', 'rainy', 'cloudy', 'sunny', 'not sunny'])
