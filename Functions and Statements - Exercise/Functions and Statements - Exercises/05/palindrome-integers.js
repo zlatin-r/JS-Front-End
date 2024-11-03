8728470f902cff984c97263a383da0a9f5d5d4a9
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-03 16:39:00 +0100

    Initial Commit

---
+++ Functions and Statements - Exercise/Functions and Statements - Exercises/05/palindrome-integers.js
@@ -0,0 +1,14 @@
+function solve(arr) {
+    const stringArr = arr.map(String);
+
+    for (number of stringArr) {
+        if (number === number.split('').reverse().join('')) {
+            console.log('true')
+        } else {
+            console.log('false')
+        }
+    }
+}
+
+solve([123,323,421,121])
+solve([32,2,232,1010])
