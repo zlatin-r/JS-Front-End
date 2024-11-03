c69f27a3081b7b024c996cfd34d59fd2b8a95af7
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-03 15:49:04 +0100

    Initial Commit

---
+++ Functions and Statements - Exercise/Functions and Statements - Exercises/01/smallest-of-three-numbers.js
@@ -0,0 +1,8 @@
+function smallestNumber(num1, num2, num3) {
+    console.log(Math.min(num1, num2, num3));
+}
+
+smallestNumber(2, 5, 3);
+smallestNumber(600, 342, 123);
+smallestNumber(25, 21, 4);
+smallestNumber(2,2,2);
