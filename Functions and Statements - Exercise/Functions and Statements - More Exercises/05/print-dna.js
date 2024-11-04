7ee8b5dd7d725c577da571fcae9e71d45c7fc8c5
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-04 17:53:31 +0100

    Solution

--- Functions and Statements - Exercise/Functions and Statements - More Exercises/05/print-dna.js
+++ Functions and Statements - Exercise/Functions and Statements - More Exercises/05/print-dna.js
@@ -1,14 +1,19 @@
-function solve(number) {
-    let pattern = 'ATCGTTAGGG'
-    let asterisk = '*'
-    let dash = '-'
+function dnaPrint(number) {
+    let str = "ATCGTTAGGG";
+    let counter = 0;
 
-    for (let i = 0; i < number; i ++) {
-        if (i === pattern.length) {
-
+    for (let i = 0; i < number; i++) {
+        if (i % 4 === 0) {
+            console.log(`**${str[counter % 10]}${str[(counter % 10) + 1]}**`);
+        } else if (i % 4 === 1) {
+            console.log(`*${str[counter % 10]}--${str[(counter % 10) + 1]}*`);
+        } else if (i % 4 === 2) {
+            console.log(`${str[counter % 10]}----${str[(counter % 10) + 1]}`);
+        } else if (i % 4 === 3) {
+            console.log(`*${str[counter % 10]}--${str[(counter % 10) + 1]}*`);
         }
-        console.log(`${pattern[i]} ${dash} ${pattern[i + 1]}`)
+        counter += 2;
     }
 }
 
-solve(10)
+dnaPrint(10)
