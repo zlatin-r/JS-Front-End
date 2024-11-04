dbfd1116c20de70ef90894965c31513d93406ee4
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-04 16:29:41 +0100

    Solution

--- Functions and Statements - Exercise/Functions and Statements - More Exercises/04/radio-cristals.js
+++ Functions and Statements - Exercise/Functions and Statements - More Exercises/04/radio-cristals.js
@@ -1,14 +1,7 @@
 function solve(arr) {
-    const targetThickness = arr[0];
-    let chunks = arr.slice(1);
+    const targetThickness = arr.shift();
+    let chunks = arr;
     let xrayUsed = false;
-
-    let cutCount = 0;
-    let lapCount = 0;
-    let grindCount = 0;
-    let etchCount = 0;
-
-    let result = []
 
     function cut(value) {
         return value /= 4;
@@ -36,49 +29,60 @@ function solve(arr) {
     }
 
     for (let currChunk of chunks) {
-        while (currChunk > targetThickness) {
+
+        let cutCount = 0;
+        let lapCount = 0;
+        let grindCount = 0;
+        let etchCount = 0;
+
+        while (currChunk !== targetThickness) {
             console.log(`Processing chunk ${currChunk} microns`);
 
             while (cut(currChunk) >= targetThickness || cut(currChunk) === targetThickness - 1) {
                 cutCount++
                 currChunk = cut(currChunk);
             }
-            console.log(`Cut x${cutCount}`)
-            currChunk = transportAndWashing(currChunk);
-            console.log('Transporting and washing');
-
+            if (cutCount) {
+                console.log(`Cut x${cutCount}`)
+                currChunk = transportAndWashing(currChunk);
+                console.log('Transporting and washing');
+            }
             while (lap(currChunk) >= targetThickness || lap(currChunk) === targetThickness - 1) {
                 lapCount++
                 currChunk = lap(currChunk);
             }
-            console.log(`Lap x${lapCount}`)
-            currChunk = transportAndWashing(currChunk);
-            console.log('Transporting and washing');
-
+            if (lapCount) {
+                console.log(`Lap x${lapCount}`)
+                currChunk = transportAndWashing(currChunk);
+                console.log('Transporting and washing');
+            }
             while (grind(currChunk) >= targetThickness || grind(currChunk) === targetThickness - 1) {
                 grindCount++
                 currChunk = grind(currChunk);
             }
-            console.log(`Grind x${grindCount}`)
-            currChunk = transportAndWashing(currChunk);
-            console.log('Transporting and washing');
-
+            if (grindCount) {
+                console.log(`Grind x${grindCount}`)
+                currChunk = transportAndWashing(currChunk);
+                console.log('Transporting and washing');
+            }
             while (etch(currChunk) >= targetThickness || etch(currChunk) === targetThickness - 1) {
                 etchCount++
                 currChunk = etch(currChunk);
             }
-            console.log(`Etch x${etchCount}`)
-            currChunk = transportAndWashing(currChunk);
-            console.log('Transporting and washing');
-
+            if (etchCount) {
+                console.log(`Etch x${etchCount}`)
+                currChunk = transportAndWashing(currChunk);
+                console.log('Transporting and washing');
+            }
             if (currChunk === targetThickness - 1 && !xrayUsed) {
                 xrayUsed = true;
-                xray(currChunk);
+                currChunk = xray(currChunk);
                 console.log(`X-ray x1`)
             }
+            console.log(`Finished crystal ${currChunk} microns`);
         }
-        console.log(`Finished crystal ${currChunk} microns`);
     }
 }
 
-solve([1000, 8100])
+solve([1000, 4000, 8100])
+// solve([1375, 50000])
