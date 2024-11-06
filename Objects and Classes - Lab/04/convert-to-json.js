b79640e470e7139f619d01d26dda4734ec9bc209
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-06 11:08:21 +0100

    Solution

---
+++ Objects and Classes - Lab/04/convert-to-json.js
@@ -0,0 +1,11 @@
+function convertToJson (name, lastName, hairColor) {
+    let person = {}
+
+    person.name = name
+    person.lastName = lastName
+    person.hairColor = hairColor
+
+    console.log(JSON.stringify(person))
+}
+
+convertToJson('George', 'Jones', 'Brown')
