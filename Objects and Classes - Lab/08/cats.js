f00e92d56bb9056dc719fde424f17ed41d844939
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-06 12:25:32 +0100

    Solution

---
+++ Objects and Classes - Lab/08/cats.js
@@ -0,0 +1,25 @@
+function solve(input) {
+    let cats = []
+
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        speak() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (let line of input) {
+        let [name, age] = line.split(' ');
+        cats.push(new Cat(name, age));
+    }
+
+    for (let cat of cats) {
+        cat.speak();
+    }
+}
+
+solve(['Candy 1', 'Poppy 3', 'Nyx 2'])
