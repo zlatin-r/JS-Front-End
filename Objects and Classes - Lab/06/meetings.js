c9e48e89411687bc9aabfeb17f3408e281b45029
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-06 11:37:43 +0100

    Solution

---
+++ Objects and Classes - Lab/06/meetings.js
@@ -0,0 +1,26 @@
+function meetings(input) {
+    let meetings = {}
+
+    for (let line of input) {
+        let tokens = line.split(' ')
+        let day = tokens[0]
+        let name = tokens[1]
+
+        if (meetings.hasOwnProperty(day)) {
+            console.log(`Conflict on ${day}!`)
+        } else {
+            meetings[day] = name
+            console.log(`Scheduled for ${day}`)
+        }
+    }
+    for (let key in meetings) {
+        console.log(`${key} -> ${meetings[key]}`);
+    }
+}
+
+meetings(['Friday Bob',
+    'Saturday Ted',
+    'Monday Bill',
+    'Monday John',
+    'Wednesday George']
+)
