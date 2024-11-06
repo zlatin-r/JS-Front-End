1d10d620a456fc69c42539aa0056dc7fabc07bb5
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-06 16:25:46 +0100

    Solution

--- Objects and Classes - Exercise/Exercises/05/inventory.js
+++ Objects and Classes - Exercise/Exercises/05/inventory.js
@@ -1,3 +1,21 @@
 function solve(input) {
+    let heroesList = [];
 
+    for (let line of input) {
+        let [heroName, heroLevel, inventory] = line.split(' / ');
+        heroesList.push({name: heroName, level: heroLevel, inventory: inventory});
+    }
+
+    heroesList.sort((a, b) => a.level - b.level);
+
+    for (let hero of heroesList) {
+        console.log(`Hero: ${hero.name}\nlevel => ${hero.level}\nitems => ${hero.inventory}`);
+    }
 }
+
+solve([
+        'Batman / 2 / Banana, Gun',
+        'Superman / 18 / Sword',
+        'Poppy / 28 / Sentinel, Antara'
+    ]
+)
