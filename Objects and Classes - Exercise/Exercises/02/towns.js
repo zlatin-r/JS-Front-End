19427211be96dce00b5f168caeb4b041bedf715a
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-06 13:27:47 +0100

    Solution

---
+++ Objects and Classes - Exercise/Exercises/02/towns.js
@@ -0,0 +1,18 @@
+function solve(input) {
+
+    for (let line of input) {
+        let [town, latitude, longitude] = line.split(' | ');
+
+        let obj = {};
+
+        obj.town = town;
+        obj.latitude = Number(latitude).toFixed(2);
+        obj.longitude = Number(longitude).toFixed(2);
+
+        console.log(obj);
+    }
+}
+
+solve(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']
+)
