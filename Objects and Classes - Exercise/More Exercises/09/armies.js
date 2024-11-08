629772844633faaaad5414c2ce924f884e62da37
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-08 17:57:50 +0100

    Solution

--- Objects and Classes - Exercise/More Exercises/09/armies.js
+++ Objects and Classes - Exercise/More Exercises/09/armies.js
@@ -34,17 +34,25 @@ function solve(input) {
         }
 
     }
+    let sortedLeaders = Object.keys(leaders)
+        .sort((a, b) => leaders[b].totalArmy - leaders[a].totalArmy);
+
+    for (let leader of sortedLeaders) {
+        console.log(`${leader}: ${leaders[leader].totalArmy}`);
+        Object.entries(leaders[leader]).sort((a, b) => b[1] - a[1])
+            .forEach(([key, value]) => {
+                if (key != "totalArmy") console.log(`>>> ${key} - ${value}`);
+            })
+    }
 }
 
-solve(['Rick Burr arrives',
-    'Fergus: Wexamp, 30245',
-    'Rick Burr: Juard, 50000',
+solve([
+    'Rick Burr arrives',
     'Findlay arrives',
-    'Findlay: Britox, 34540',
-    'Wexamp + 6000', 'Juard + 1350',
-    'Britox + 4500', 'Porter arrives',
-    'Porter: Legion, 55000',
-    'Legion + 302',
-    'Rick Burr defeated',
-    'Porter: Retix, 3205']
-)
+    'Rick Burr: Juard, 1500',
+    'Wexamp arrives',
+    'Findlay: Wexamp, 34540',
+    'Wexamp + 340',
+    'Wexamp: Britox, 1155',
+    'Wexamp: Juard, 43423'
+])
