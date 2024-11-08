f3224f4f9cd770c508713cd0596e60e7859f92b3
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-08 15:44:29 +0100

    Solution

--- Objects and Classes - Exercise/More Exercises/08/garage.js
+++ Objects and Classes - Exercise/More Exercises/08/garage.js
@@ -2,32 +2,28 @@ function solve(input) {
     let garages = {};
 
     for (let line of input) {
-        let garageNumber = Number(line[0]);
+        let [garageInfo, carInfo] = line.split(' - ');
+        let garageNumber = Number(garageInfo.trim());
 
         if (!garages.hasOwnProperty(garageNumber)) {
-            garages[garageNumber] = {};
+            garages[garageNumber] = [];
         }
-        let data = line.slice(4).split(', ');
-
-        for (let carInfo of data) {
-            let [key, value] = carInfo.split(': ');
-            garages[garageNumber][key] = value;
-        }
+        carInfo = carInfo.replace(/:/g, ' -')
+        garages[garageNumber].push(carInfo);
     }
 
     for (let garageNumber in garages) {
         console.log(`Garage № ${garageNumber}`);
         let cars = garages[garageNumber];
-        for (let key in cars) {
-            console.log(`--- ${key} - ${cars[key]}`);
+        for (let car of cars) {
+            console.log(`--- ${car}`);
         }
     }
-
 }
 
 solve([
     '1 - color: blue, fuel type: diesel',
     '1 - color: red, manufacture: Audi',
     '2 - fuel type: petrol',
-    '4 - color: dark blue, fuel type: diesel, manufacture: Fiat']
-)
+    '4 - color: dark blue, fuel type: diesel, manufacture: Fiat'
+]);
