696129640400ca0a7be146a72b581142009ee09e
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-08 15:01:06 +0100

    Solution

--- Objects and Classes - Exercise/More Exercises/07/sequences-2.js
+++ Objects and Classes - Exercise/More Exercises/07/sequences-2.js
@@ -1,20 +1,20 @@
-function solve(input) {
-    let result = [];
+function uniqueSequences(stringsArray) {
+    let uniqueArrays = []
 
-    for (let list of input) {
-        let numList = JSON.parse(list).sort((a, b) => b - a);
-        let strList = JSON.stringify(numList).split(',').join(', ')
-
-        if (!result.includes(strList)) {
-            result.push(strList);
+    for (let data of stringsArray) {
+        data = JSON.parse(data).sort((a, b) => b - a);
+        if (uniqueArrays.length === 0) uniqueArrays.push(data);
+        else if (!(JSON.stringify(uniqueArrays).includes(JSON.stringify(data)))) {
+            uniqueArrays.push(data)
         }
+
     }
 
-    let sortedResult = result.sort((a, b) => a.length - b.length);
-    console.log(sortedResult.join('\n'));
+    for (let element of uniqueArrays.sort((a, b) => a.length - b.length)) {
+        console.log(`[${element.join(", ")}]`);
+    }
 }
 
-solve(["[7.14, 7.180, 7.339, 80.099]",
-    "[7.339, 80.0990, 7.140000, 7.18]",
-    "[7.339, 7.180, 7.14, 80.099]"]
-)
+uniqueSequences(["[-3, -2, -1, 0, 1, 2, 3, 4]",
+"[10, 1, -17, 0, 2, 13]",
+"[4, -3, 3, -2, 2, -1, 1, 0]"])
--- Objects and Classes - Exercise/More Exercises/07/sequences.js
+++ Objects and Classes - Exercise/More Exercises/07/sequences.js
@@ -2,19 +2,19 @@ function solve(input) {
     let result = [];
 
     for (let list of input) {
-        let numList = JSON.parse(list).sort((a, b) => b - a);
-        let strList = JSON.stringify(numList).split(',').join(', ')
+        list = JSON.parse(list).sort((a, b) => b - a);
 
-        if (!result.includes(strList)) {
-            result.push(strList);
+        if (!JSON.stringify(result).includes(JSON.stringify(list))) {
+            result.push(list);
         }
     }
 
-    let sortedResult = result.sort((a, b) => a.length - b.length);
-    console.log(sortedResult.join('\n'));
+    for (let el of result.sort((a,b) => a.length - b.length)) {
+        console.log(`[${el.join(", ")}]`);
+    }
 }
 
-solve(["[7.14, 7.180, 7.339, 80.099]",
-    "[7.339, 80.0990, 7.140000, 7.18]",
-    "[7.339, 7.180, 7.14, 80.099]"]
+solve(["[-3, -2, -1, 0, 1, 2, 3, 4]",
+    "[10, 1, -17, 0, 2, 13]",
+    "[4, -3, 3, -2, 2, -1, 1, 0]"]
 )
