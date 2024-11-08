dd02152f7d98dd9425317cf823c9c17f66fdad9f
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-08 14:05:25 +0100

    Solution

--- Objects and Classes - Exercise/More Exercises/06/browser-history.js
+++ Objects and Classes - Exercise/More Exercises/06/browser-history.js
@@ -36,12 +36,9 @@ function solve(browserData, commands) {
     console.log(`Browser Logs: ${browserData['Browser Logs'].join(', ')}`);
 }
 
-solve({
-        "Browser Name": "Google Chrome",
-        "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
-        "Recently Closed": ["Yahoo", "Gmail"],
-        "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate",
-            "Close Yahoo", "Open Gmail", "Close Gmail", "Open Facebook"]
-    },
-    ["Close Facebook", "Open StackOverFlow", "Open Google"]
+solve({"Browser Name":"Mozilla Firefox",
+    "Open Tabs":["YouTube"],
+    "Recently Closed":["Gmail", "Dropbox"],
+    "Browser Logs":["Open Gmail", "Close Gmail", "Open Dropbox", "Open YouTube", "Close Dropbox"]},
+    ["Open Wikipedia", "Clear History and Cache", "Open Twitter"]
 )
