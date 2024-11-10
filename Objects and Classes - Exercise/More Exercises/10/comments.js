dcbc95fdd5c0f64c7807927f2ff62fb28b2e57a4
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-10 15:12:44 +0100

    Solution

---
+++ Objects and Classes - Exercise/More Exercises/10/comments.js
@@ -0,0 +1,32 @@
+function solve(input) {
+    let users = [];
+    let articles = [];
+    let information = {};
+
+    for (let line of input) {
+        if (line.includes('user')) {
+            let userName = line.slice(5)
+            information[userName] = {};
+        } else if (line.includes('article')) {
+            let article = line.slice(8)
+            console.log(article);
+        } else {
+            let [userName]
+        }
+    }
+}
+
+solve([
+    'user aUser123',
+    'someUser posts on someArticle: NoTitle, stupidComment',
+    'article Books',
+    'article Movies',
+    'article Shopping',
+    'user someUser',
+    'user uSeR4',
+    'user lastUser',
+    'uSeR4 posts on Books: I like books, I do really like them',
+    'uSeR4 posts on Movies: I also like movies, I really do',
+    'someUser posts on Shopping: title, I go shopping every day',
+    'someUser posts on Movies: Like, I also like movies very much'
+])
