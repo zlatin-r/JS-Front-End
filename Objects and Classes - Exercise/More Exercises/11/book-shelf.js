a97b9cdae2db6fa629f41bc88a059dc76b4589f7
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-10 16:30:33 +0100

    Solution

---
+++ Objects and Classes - Exercise/More Exercises/11/book-shelf.js
@@ -0,0 +1,20 @@
+function solve(input) {
+
+}
+
+solve([
+    '1 -> history',
+    '1 -> action',
+    'Death in Time: Criss Bell, mystery',
+    '2 -> mystery',
+    '3 -> sci-fi',
+    'Child of Silver: Bruce Rich, mystery',
+    'Hurting Secrets: Dustin Bolt, action',
+    'Future of Dawn: Aiden Rose, sci-fi',
+    'Lions and Rats: Gabe Roads, history',
+    '2 -> romance',
+    'Effect of the Void: Shay B, romance',
+    'Losing Dreams: Gail Starr, sci-fi',
+    'Name of Earth: Jo Bell, sci-fi',
+    'Pilots of Stone: Brook Jay, history'
+])
