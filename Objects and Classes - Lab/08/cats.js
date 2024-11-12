5d45a6bd489747e6c86d0f38222e8a713d497822
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-12 16:40:32 +0100

    Solution

--- Objects and Classes - Lab/08/cats.js
+++ Objects and Classes - Lab/08/cats.js
@@ -1,5 +1,5 @@
 function solve(input) {
-    let cats = []
+    let cats = [];
 
     class Cat {
         constructor(name, age) {
@@ -16,10 +16,13 @@ function solve(input) {
         let [name, age] = line.split(' ');
         cats.push(new Cat(name, age));
     }
-
     for (let cat of cats) {
         cat.speak();
     }
 }
 
-solve(['Candy 1', 'Poppy 3', 'Nyx 2'])
+solve([
+    'Candy 1',
+    'Poppy 3',
+    'Nyx 2'
+]);
