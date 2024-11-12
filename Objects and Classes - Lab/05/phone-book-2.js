a730cdcace3fafe6d7497fdd1fe63bb7f12a8ec4
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-12 15:24:15 +0100

    Solution

---
+++ Objects and Classes - Lab/05/phone-book-2.js
@@ -0,0 +1,18 @@
+function phoneBook(input) {
+    const phoneBook = {};
+
+    input.forEach((item) => {
+        const [name, number] = item.split(' ');
+        phoneBook[name] = number;
+    })
+
+    for (let key in phoneBook) {
+        console.log(`${key} -> ${phoneBook[key]}`);
+    }
+}
+
+phoneBook(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']
+)
