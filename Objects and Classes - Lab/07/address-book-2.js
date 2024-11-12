3162e65d2dabd9b7ff374198ed10d1187048ff29
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-12 16:25:21 +0100

    Solution

--- Objects and Classes - Lab/07/address-book-2.js
+++ Objects and Classes - Lab/07/address-book-2.js
@@ -1,11 +1,19 @@
 function solve(input) {
-    const entries = Object.entries(input);
-    entries.sort((a, b) => a.localeCompare(b));
+    const addressBook = {};
 
-    entries.forEach(([k, v]) =>
-    {
-        console.log(k,v)
-    })
+    input.forEach((entry) => {
+        const [name, place] = entry.split(':');
+        addressBook[name] = place;
+    });
+
+    const addressBookSortable = Object.entries(addressBook);
+    addressBookSortable.sort(([keyA], [keyB]) => {
+        return keyA.localeCompare(keyB);
+    });
+
+    for (const [key, value] of addressBookSortable) {
+        console.log(`${key} -> ${value}`);
+    }
 }
 
 solve(['Tim:Doe Crossing',
