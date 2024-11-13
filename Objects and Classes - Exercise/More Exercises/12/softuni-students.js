1a207f99a44bb3a173a206ab1e31213d445e1847
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-13 11:43:58 +0100

    Solution

---
+++ Objects and Classes - Exercise/More Exercises/12/softuni-students.js
@@ -0,0 +1,36 @@
+function solve(input) {
+    let courses = {};
+    let courseName = '';
+    let capacity = 0;
+    let userName = '';
+    let studentEmail = '';
+    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+
+    for (let line of input) {
+        if (line.includes('email')) {
+            studentEmail = line.match(emailRegex)
+            userName = line.split(' ')[0];
+            courseName = line.split(' ').pop();
+
+        }
+    }
+
+}
+
+solve([
+    'JavaBasics: 2',
+    'user1[25] with email user1@user.com joins C#Basics',
+    'C#Advanced: 3',
+    'JSCore: 4',
+    'user2[30] with email user2@user.com joins C#Basics',
+    'user13[50] with email user13@user.com joins JSCore',
+    'user1[25] with email user1@user.com joins JSCore',
+    'user8[18] with email user8@user.com joins C#Advanced',
+    'user6[85] with email user6@user.com joins JSCore',
+    'JSCore: 2',
+    'user11[3] with email user11@user.com joins JavaBasics',
+    'user45[105] with email user45@user.com joins JSCore',
+    'user007[20] with email user007@user.com joins JSCore',
+    'user700[29] with email user700@user.com joins JSCore',
+    'user900[88] with email user900@user.com joins JSCore'
+]);
