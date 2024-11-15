14cdea9c6610b1e704eb3a87d0815ca2f96fa871
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 14:15:14 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/05. Table - Search Engine/table-search-engine.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,12 @@
 function solve() {
-   //TODO
+    const searchedElement = document.querySelector('#searchField').value;
+    const rowElements = document.querySelectorAll('table:not(tr:first-child) tr td');
+
+    console.log(searchedElement)
+    rowElements.forEach(el => {
+
+        if (el.textContent.includes(searchedElement)) {
+            el.style.backgroundColor = 'yellow';
+        }
+    })
 }
