c1a1aff1a1ab92a6401d2f5775b15020154bc4d3
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 12:57:08 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -3,20 +3,46 @@ function solve() {
     let text = document.querySelector('#text').value.toLowerCase();
     let result = "";
 
-    if (namingConvention === 'Camel Case') {
+    function camelCase(text) {
         for (let word of text.split(' ')) {
-            word = word.charAt(0).toUpperCase() + word.substring(1);
-            result += word;
+            result += word.charAt(0).toUpperCase() + word.substring(1);
         }
-        result = result.charAt(0).toLowerCase() + result.slice(1);
-    } else if (namingConvention === 'Pascal Case') {
-        for (let word of text.split(' ')) {
-            word = word.charAt(0).toUpperCase() + word.substring(1);
-            result += word;
-        }
-    } else {
-        result = 'Error!';
+        return result.charAt(0).toLowerCase() + result.slice(1);
     }
 
+    function pascalCase(text) {
+        for (let word of text.split(' ')) {
+            result += word.charAt(0).toUpperCase() + word.substring(1);
+        }
+        return result
+    }
+
+    if (namingConvention === 'Camel Case') {
+        result = camelCase(text);
+    } else if (namingConvention === 'Pascal Case') {
+        result = pascalCase(text);
+    } else {
+        result = 'Error!'
+    }
     document.querySelector('#result').textContent = result;
 }
+
+// function solve() {
+//     const namingConvention = document.querySelector('#naming-convention').value;
+//     let text = document.querySelector('#text').value.toLowerCase();
+//     let result = "";
+//
+//     if (namingConvention === 'Camel Case') {
+//         for (let word of text.split(' ')) {
+//             result += word.charAt(0).toUpperCase() + word.substring(1);
+//         }
+//         result = result.charAt(0).toLowerCase() + result.slice(1);
+//     } else if (namingConvention === 'Pascal Case') {
+//         for (let word of text.split(' ')) {
+//             result += word.charAt(0).toUpperCase() + word.substring(1);
+//         }
+//     } else {
+//         result = 'Error!'
+//     }
+//     document.querySelector('#result').textContent = result;
+// }
