cd1049c87146753e68049640ba4a7b8a8991dc31
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 11:15:06 +0100

    Solution

--- DOM Introduction - Lab/DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
+++ DOM Introduction - Lab/DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
@@ -1,21 +1,22 @@
-// function sumTable() {
-//     const prices = document.querySelectorAll
-//     ("table tbody tr:nth-child(-n+3) td:nth-of-type(even)");
-//     let sum = 0;
-//
-//     prices.forEach(price => {
-//         const value = parseFloat(price.textContent);
-//         sum += value;
-//     });
-//
-//     document.querySelector("#sum").textContent = sum.toFixed(2);
-// }
-
 function sumTable() {
-    const tableCells = document.querySelectorAll('table tbody tr:not(:last-child) td:last-child');
-    const sumElement = document.querySelector('#sum');
+    const prices = document.querySelectorAll
+    ("table tbody tr:nth-child(-n+3) td:nth-of-type(even)"); // This select didn't work in judge
+    const sumElement = document.querySelector("#sum");
+    let sum = 0;
 
-    sumElement.textContent = [...tableCells]
-        .map(el => Number(el.textContent))
-        .reduce((a, b) => a + b, 0);
+    prices.forEach(price => {
+        const value = parseFloat(price.textContent);
+        sum += value;
+    });
+
+    sumElement.textContent = sum;
 }
+
+// function sumTable() {
+//     const tableCells = document.querySelectorAll('table tbody tr:not(:last-child) td:last-child');
+//     const sumElement = document.querySelector('#sum');
+//
+//     sumElement.textContent = [...tableCells]
+//         .map(el => Number(el.textContent))
+//         .reduce((a, b) => a + b, 0);
+// }
