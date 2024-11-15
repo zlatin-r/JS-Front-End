bf42ebaadd12a6ab4c717cd24463c0e363824952
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 11:09:04 +0100

    Solution

--- DOM Introduction - Lab/DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
+++ DOM Introduction - Lab/DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
@@ -1,3 +1,21 @@
+// function sumTable() {
+//     const prices = document.querySelectorAll
+//     ("table tbody tr:nth-child(-n+3) td:nth-of-type(even)");
+//     let sum = 0;
+//
+//     prices.forEach(price => {
+//         const value = parseFloat(price.textContent);
+//         sum += value;
+//     });
+//
+//     document.querySelector("#sum").textContent = sum.toFixed(2);
+// }
+
 function sumTable() {
-    // TODO
+    const tableCells = document.querySelectorAll('table tbody tr:not(:last-child) td:last-child');
+    const sumElement = document.querySelector('#sum');
+
+    sumElement.textContent = [...tableCells]
+        .map(el => Number(el.textContent))
+        .reduce((a, b) => a + b, 0);
 }
