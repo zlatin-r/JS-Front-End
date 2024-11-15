687244c7a917496fdfc220761328bcc2852ecb2a
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 12:42:47 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+    const namingConvention = document.querySelector('#naming-convention').value;
+    let text = document.querySelector('#text').value.toLowerCase();
+    let result = "";
+
+    if (namingConvention === 'Camel Case') {
+        for (let word of text.split(' ')) {
+            word = word.charAt(0).toUpperCase() + word.substring(1);
+            result += word;
+        }
+        result = result.charAt(0).toLowerCase() + result.slice(1);
+    } else if (namingConvention === 'Pascal Case') {
+        for (let word of text.split(' ')) {
+            word = word.charAt(0).toUpperCase() + word.substring(1);
+            result += word;
+        }
+    } else {
+        result = 'Error!';
+    }
+
+    document.querySelector('#result').textContent = result;
 }
