7ab3d1699883a8e1a36bc1267bfb2df1a764b0bc
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 13:50:04 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/04. Search in List/search-in-list.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/04. Search in List/search-in-list.js
@@ -1,3 +1,11 @@
 function solve() {
-   // TODO
+    const townNameElements = document.querySelectorAll("#towns li");
+    const searchedElement = document.querySelector("#searchText").value;
+    console.log(townNameElements);
+    console.log(searchedElement);
+
+    townNameElements.forEach(town => {
+        const townName = town.textContent.trim();
+        console.log(townName);
+    })
 }
