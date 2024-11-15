b4d2031e5d4b127c1ffec4aad8d4d2e6ba613daa
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 10:05:01 +0100

    Solution

--- DOM Introduction - Lab/DOM-Introduction-Lab-Resources/03.Sum Numbers/sum-numbers.js
+++ DOM Introduction - Lab/DOM-Introduction-Lab-Resources/03.Sum Numbers/sum-numbers.js
@@ -1,3 +1,8 @@
 function calc() {
-    // TODO
+    const numOne = document.getElementsByTagName('input')[0].value;
+    const numTwo = document.getElementsByTagName('input')[1].value;
+
+    const result = Number(numOne) + Number(numTwo);
+
+    document.getElementsByTagName('input')[2].value = result;
 }
