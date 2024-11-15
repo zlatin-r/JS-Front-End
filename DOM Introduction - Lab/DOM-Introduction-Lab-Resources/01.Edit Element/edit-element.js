7525051eb92c3ab2091b6d34e3e201e8a6af4496
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 09:20:16 +0100

    Initial Commit

--- DOM Introduction - Lab/DOM-Introduction-Lab-Resources/01.Edit Element/edit-element.js
+++ DOM Introduction - Lab/DOM-Introduction-Lab-Resources/01.Edit Element/edit-element.js
@@ -1,5 +1,6 @@
 function editElement(el, match, replace) {
-    const htmlElement = document.querySelector(`${match}`);
-
-    console.log(htmlElement);
+    const htmlElement = el.textContent;
+    const matcher = new RegExp(match, 'g');
+    const edited = htmlElement.replace(matcher, replace);
+    el.textContent = edited;
 }
