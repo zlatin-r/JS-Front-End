896e3fa5a0c809d1b6bf8207935c680e8882da07
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 16:40:42 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/05. Table - Search Engine/table-search-engine.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/05. Table - Search Engine/table-search-engine.js
@@ -1,12 +1,30 @@
+// function solve() {
+//    document.querySelector('#searchBtn').addEventListener('click', onClick);
+//
+//    function onClick() {
+//       const searchPattern = document.getElementById("searchField").value;
+//       const dataCells = Array.from(document.querySelectorAll("tbody td"));
+//
+//       const rows = Array.from(document.querySelectorAll("tbody tr"))
+//
+//       rows.forEach(row => row.classList.remove("select"));
+//
+//       dataCells.forEach(cell => {
+//          if (cell.textContent.includes(searchPattern)) {
+//             cell.parentElement.classList.add("select")
+//          };
+//       });
+//    };
+// }
+
 function solve() {
     const searchedElement = document.querySelector('#searchField').value;
-    const rowElements = Array.from(document.querySelectorAll('tbody td'));
-
+    const allCells = Array.from(document.querySelectorAll('tbody td'));
     const rows = Array.from(document.querySelectorAll('tbody tr'));
 
     rows.forEach(row => row.classList.remove("select"));
 
-    rowElements.forEach(cell => {
+    allCells.forEach(cell => {
         if (cell.textContent.includes(searchedElement)) {
             cell.parentElement.classList.add("select");
         }
