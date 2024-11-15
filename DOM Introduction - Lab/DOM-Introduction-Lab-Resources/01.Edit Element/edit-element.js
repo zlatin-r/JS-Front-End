1c96bc707af377f08abef34413763bf28daecc9a
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-15 09:25:41 +0100

    Solution

--- DOM Introduction - Lab/DOM-Introduction-Lab-Resources/01.Edit Element/edit-element.js
+++ DOM Introduction - Lab/DOM-Introduction-Lab-Resources/01.Edit Element/edit-element.js
@@ -1,6 +1,10 @@
+// function editElement(el, match, replacer) {
+//     const content = el.textContent;
+//     const matcher = new RegExp(match, 'g');
+//     const edited = content.replace(matcher, replacer);
+//     el.textContent = edited;
+// }
+
 function editElement(el, match, replacer) {
-    const content = el.textContent;
-    const matcher = new RegExp(match, 'g');
-    const edited = content.replace(matcher, replacer);
-    el.textContent = edited;
+    el.textContent = el.textContent.replaceAll(match, replacer);
 }
