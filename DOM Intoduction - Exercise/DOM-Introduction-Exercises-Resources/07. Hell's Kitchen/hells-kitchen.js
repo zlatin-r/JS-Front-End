fc6cadf1157d06886bedc4e9db3f5f82d8afd1e9
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 16:45:12 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/07. Hell's Kitchen/hells-kitchen.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/07. Hell's Kitchen/hells-kitchen.js
@@ -1,3 +1,36 @@
 function solve() {
-    //TODO
+    const inputData = JSON.parse(document.querySelector("#inputs textarea").value);
+    console.log(inputData);
+    const restaurantsData = {};
+
+    inputData.forEach(element => {
+        const [restaurantName, personalInfo] = element.split(' - ');
+        const employeesData = personalInfo.split(', ');
+
+        if (!restaurantsData.hasOwnProperty(restaurantName)) {
+            restaurantsData[restaurantName] = {};
+
+            employeesData.forEach(employee => {
+                const [name, salary] = employee.split(' ');
+
+                if (!restaurantsData[restaurantName].hasOwnProperty(name)) {
+                    restaurantsData[restaurantName][name] = parseFloat(salary);
+                } else {
+                    restaurantsData[restaurantName][name] += parseFloat(salary);
+                }
+            })
+        } else {
+            employeesData.forEach(employee => {
+                const [name, salary] = employee.split(' ');
+
+                if (!restaurantsData[restaurantName].hasOwnProperty(name)) {
+                    restaurantsData[restaurantName][name] = parseFloat(salary);
+                } else {
+                    restaurantsData[restaurantName][name] += parseFloat(salary);
+                }
+            })
+        }
+    })
+
+    console.log(inputData);
 }
