701a33aac5b12875ee569a09bf5e2777a09e0d8c
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 16:05:52 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -8,7 +8,8 @@ function solve() {
     for (let i = 0; i < sentences.length; i += 3) {
         const group = sentences.slice(i, i + 3);
         const newParagraphElement = document.createElement('p');
-        newParagraphElement.textContent = group.map(el => `${el}.`).join('')
+
+        newParagraphElement.textContent = group.map(el => `${el}.`).join('');
         outputElement.appendChild(newParagraphElement);
     }
 }
