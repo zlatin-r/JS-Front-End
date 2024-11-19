c16e3e4daa3fe001cd6806d69c90fab1a47449cc
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 16:15:31 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -1,15 +1,32 @@
+// function solve() {
+//     const textElement = document.querySelector('#input').value;
+//     const outputElement = document.querySelector('#output');
+//     let sentences = textElement.split(".");
+//
+//     sentences = sentences.filter(el => el.length > 0).map(el => el += ".");
+//
+//     for (let i = 0; i < sentences.length; i += 3) {
+//         const group = sentences.slice(i, i + 3);
+//         const newParagraphElement = document.createElement('p');
+//
+//         newParagraphElement.textContent = group.join("").trim();
+//         outputElement.appendChild(newParagraphElement);
+//     }
+// }
+
+
 function solve() {
-    const outputElement = document.querySelector('#output');
     const textElement = document.querySelector('#input').value;
+    const outputElement = document.querySelector('#output');
     let sentences = textElement
         .split(".")
-        .filter(s => s.trim().length > 0);
+        .filter(s => s.length > 0)
+        .map(s => s + ".");
 
     for (let i = 0; i < sentences.length; i += 3) {
         const group = sentences.slice(i, i + 3);
         const newParagraphElement = document.createElement('p');
-
-        newParagraphElement.textContent = group.map(el => `${el}.`).join('');
+        newParagraphElement.textContent = group.join('').trim();
         outputElement.appendChild(newParagraphElement);
     }
 }
