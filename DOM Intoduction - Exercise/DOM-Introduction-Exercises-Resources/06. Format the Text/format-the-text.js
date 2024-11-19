8f4eb4fe426d607b6417f365126e31cad22abd62
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 16:19:31 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -9,6 +9,7 @@ function solve() {
     for (let i = 0; i < sentences.length; i += 3) {
         const group = sentences.slice(i, i + 3);
         const newParagraphElement = document.createElement('p');
+
         newParagraphElement.textContent = group.join('').trim();
         outputElement.appendChild(newParagraphElement);
     }
@@ -31,3 +32,17 @@ function solve() {
 // }
 
 
+// function solve() {
+//   const rawText = document.querySelector("textarea").value;
+//   const outputElement = document.getElementById("output");
+//
+//   let sentences = rawText.split(".");
+//   sentences = sentences.filter(element => element.length > 0)
+//     .map(element => element += ".");
+//
+//   while (sentences.length > 0) {
+//     let paragraph = document.createElement("p");
+//     paragraph.textContent = sentences.splice(0, 3).join("").trim();
+//     outputElement.appendChild(paragraph);
+//   }
+// }
