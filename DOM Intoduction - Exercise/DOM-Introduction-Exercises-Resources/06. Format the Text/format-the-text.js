53af8e99cc4e0a5f5aa4bcef22cb9a9158fc8729
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 16:02:57 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -3,13 +3,12 @@ function solve() {
     const textElement = document.querySelector('#input').value;
     let sentences = textElement
         .split(".")
-        .filter(s => s.trim().length > 0)
-        .map(s => `<p>${s}</p>`);
+        .filter(s => s.trim().length > 0);
 
     for (let i = 0; i < sentences.length; i += 3) {
         const group = sentences.slice(i, i + 3);
         const newParagraphElement = document.createElement('p');
-        newParagraphElement.textContent = group.join('. ')
+        newParagraphElement.textContent = group.map(el => `${el}.`).join('')
         outputElement.appendChild(newParagraphElement);
     }
 }
