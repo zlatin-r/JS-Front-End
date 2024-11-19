b4f3fae64f25ef4296583e9e260b2b78828c0af2
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 15:08:12 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -1,10 +1,15 @@
 function solve() {
+    const outputElement = document.querySelector('#output');
+    const newParagraphElement = document.createElement('p');
     const textElement = document.querySelector('#input').value;
     let sentences = textElement
         .split(".")
         .map(s => s.trim())
         .filter(s => s.length > 0);
 
+    if (sentences.length <= 3) {
+
+    }
     console.log(sentences.length);
     console.log(sentences);
 
