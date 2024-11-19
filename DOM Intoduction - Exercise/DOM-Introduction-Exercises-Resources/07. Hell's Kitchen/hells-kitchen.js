c791d44c96c3a252d454bd27d08d89f84ea378bf
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-19 17:19:40 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/07. Hell's Kitchen/hells-kitchen.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/07. Hell's Kitchen/hells-kitchen.js
@@ -1,15 +1,16 @@
 function solve() {
     const inputData = JSON.parse(document.querySelector("#inputs textarea").value);
-    console.log(inputData);
     const restaurantsData = {};
+    const bestRestaurant = {restaurantName: "", avrSalary: 0, highestSalary: 0, personal: {}};
 
     inputData.forEach(element => {
         const [restaurantName, personalInfo] = element.split(' - ');
         const employeesData = personalInfo.split(', ');
-        debugger
+
         if (!restaurantsData.hasOwnProperty(restaurantName)) {
             restaurantsData[restaurantName] = {};
         }
+
         employeesData.forEach(employee => {
             const [name, salary] = employee.split(' ');
 
@@ -19,6 +20,17 @@ function solve() {
                 restaurantsData[restaurantName][name] += parseFloat(salary);
             }
         });
-    });
+        const salaries = Object.values(restaurantsData[restaurantName]);
+        const totalSalaries = salaries.reduce((sum, salary) => sum + salary, 0);
+        const avrSalary = parseFloat(totalSalaries / salaries.length).toFixed(2);
+        const highestSalary = Math.max(...salaries)
 
+        if (bestRestaurant.avrSalary < avrSalary) {
+            bestRestaurant.restaurantName = restaurantName;
+            bestRestaurant.avrSalary = avrSalary;
+            bestRestaurant.highestSalary = highestSalary;
+            bestRestaurant.personal = restaurantsData[restaurantName];
+        }
+    });
+    console.log(bestRestaurant);
 }
