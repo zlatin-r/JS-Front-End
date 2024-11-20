ad9ad421972ab7ad089ba25980669c1b7ef610d7
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-20 16:53:31 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/08. Generate Report/generate-report.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/08. Generate Report/generate-report.js
@@ -1,6 +1,23 @@
+// Lector's solution
+
 function solve() {
-    // Select all checked table heads (th)
-    const checkedColumns = document.querySelectorAll("th input[type=checkbox]:checked")
-    console.log(checkedColumns)
-    // Collect data from all checked columns in JSON for each row
+
+    const outputEl = document.querySelector('#output');
+
+    const tHeadRow = document.querySelector('table thead tr');
+    const tBodyRows = document.querySelectorAll('table tbody tr');
+
+    const checkedInputElements = [...tHeadRow.children]
+        .map((th, i) => ({el: th.children[0], name: th.children[0].getAttribute('name'), index: i}))
+        .filter(object => object.el.checked);
+
+    const outputData = [...tBodyRows]
+        .map(row => {
+            return checkedInputElements.reduce((acc, input) => {
+                acc[input.name] = row.children[input.index].textContent;
+                return acc;
+            }, {});
+        });
+
+    outputEl.value = JSON.stringify(outputData);
 }
