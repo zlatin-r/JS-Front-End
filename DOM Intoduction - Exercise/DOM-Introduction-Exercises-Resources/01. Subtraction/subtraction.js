1eff32ff86e21d5efa417ccf6b6aa9f15103942b
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-20 11:57:51 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/01. Subtraction/subtraction.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/01. Subtraction/subtraction.js
@@ -4,3 +4,8 @@ function subtract() {
 
     document.querySelector('#result').textContent = firstNum - secondNum;
 }
+
+// function subtract() {
+//     const [num1, num2] = [...document.querySelectorAll('input[type="text"]')].map(el => Number(el.value));
+//     document.querySelector("#result").textContent = num1 - num2;
+// }
