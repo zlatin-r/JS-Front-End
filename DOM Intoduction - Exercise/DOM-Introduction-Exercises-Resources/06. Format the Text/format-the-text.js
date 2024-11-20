02038de28fa95568264d78c8e3021adb40f42c34
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-20 14:36:54 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -16,6 +16,32 @@ function solve() {
 }
 
 
+// Lector solution
+//
+// function solve() {
+//     const inputEl = document.querySelector('#input');
+//     const outputEl = document.querySelector('#output');
+//
+//     const sentences = inputEl.value.split('. ');
+//     const sentPerPar = 3;
+//
+//     const numberOfParagraphs = Math.ceil(sentences.length / sentPerPar);
+//
+//     let output = '';
+//
+//     for( let i = 0; i < numberOfParagraphs; i++) {
+//         const start = i * sentPerPar;
+//         const end = start + sentPerPar;
+//
+//         output += '<p>';
+//         output += sentences.slice(start, end).join('. ');
+//         output += '</p>';
+//     }
+//
+//     outputEl.innerHTML = output;
+// }
+
+
 // function solve() {
 //     const textElement = document.querySelector('#input').value;
 //     const outputElement = document.querySelector('#output');
