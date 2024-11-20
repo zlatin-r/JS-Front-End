bda0876c58d7817c138ce8f9f7fd669214f18afe
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-20 16:05:33 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/08. Generate Report/generate-report.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/08. Generate Report/generate-report.js
@@ -1,3 +1,6 @@
 function solve() {
-    //TODO
+    // Select all checked table heads (th)
+    const checkedColumns = document.querySelectorAll("th input[type=checkbox]:checked")
+    console.log(checkedColumns)
+    // Collect data from all checked columns in JSON for each row
 }
