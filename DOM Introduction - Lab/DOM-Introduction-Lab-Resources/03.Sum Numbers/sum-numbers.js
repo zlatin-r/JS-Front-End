63c2729c865bbc0cf9430596ec8686d215531ec6
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-20 11:36:25 +0100

    Solution

--- DOM Introduction - Lab/DOM-Introduction-Lab-Resources/03.Sum Numbers/sum-numbers.js
+++ DOM Introduction - Lab/DOM-Introduction-Lab-Resources/03.Sum Numbers/sum-numbers.js
@@ -3,6 +3,7 @@ function calc() {
     const numTwo = document.getElementsByTagName('input')[1].value;
 
     const result = Number(numOne) + Number(numTwo);
-
+    // document.querySelector('#sum').value = result;
+    // document.querySelector('#sum').value = numOne + numTwo;
     document.getElementsByTagName('input')[2].value = result;
 }
