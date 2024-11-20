65731a3245785861cc2976a0989ecb46ce9113bb
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-20 12:37:47 +0100

    Solution

--- DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/03. Accordion/accordion.js
+++ DOM Intoduction - Exercise/DOM-Introduction-Exercises-Resources/03. Accordion/accordion.js
@@ -9,3 +9,18 @@ function toggle() {
         document.getElementsByClassName('button')[0].textContent = "More";
     }
 }
+
+
+// function toggle() {
+//
+//     const buttonEl = document.querySelector('#accordion .button');
+//     const contentEl = document.querySelector('#extra');
+//
+//     if ( contentEl.style.display == 'block' ) {
+//         contentEl.style.display = 'none';
+//         buttonEl.textContent = 'More';
+//     } else {
+//         contentEl.style.display = 'block';
+//         buttonEl.textContent = 'Less';
+//     }
+// }
