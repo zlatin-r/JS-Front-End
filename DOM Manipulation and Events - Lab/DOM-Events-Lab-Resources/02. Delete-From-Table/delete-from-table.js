100c8eb183f09bfe5c6e644d4836a338d6006ccb
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-22 14:39:25 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/02. Delete-From-Table/delete-from-table.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/02. Delete-From-Table/delete-from-table.js
@@ -41,3 +41,19 @@ function deleteByEmail() {
 //     }
 //     inputEl.value = '';
 // }
+
+
+// function deleteByEmail() {
+//     let email = document.getElementsByName("email")[0].value;
+//     let secondColumn = document.querySelectorAll(
+//         "#customers tr td:nth-child(2)");
+//     for (let td of secondColumn)
+//         if (td.textContent === email) {
+//             let row = td.parentNode;
+//             row.parentNode.removeChild(row);
+//             document.getElementById('result').
+//                 textContent = "Deleted.";
+//             return;
+//         }
+//     document.getElementById('result').textContent = "Not found.";
+// }
