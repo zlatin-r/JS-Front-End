3f8ecaf3895b49feb908d6e048842926247411fe
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-22 15:18:54 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/03. Add-Delete/add-delete.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/03. Add-Delete/add-delete.js
@@ -1,10 +1,24 @@
 function addItem() {
+
+    function deleteItem(e) {
+        e.currentTarget.parentElement.remove();
+    }
+
     const listElement = document.querySelector('#items');
     const inputElement = document.querySelector('#newItemText');
     const inputText = inputElement.value;
+
     const newListElement = document.createElement('li');
+    const deleteButtonElement = document.createElement('a');
 
     newListElement.textContent = inputText;
+    deleteButtonElement.textContent = '[Delete]';
+    deleteButtonElement.setAttribute('href', '#');
+
+    deleteButtonElement.addEventListener('click', deleteItem);
+
+    newListElement.appendChild(deleteButtonElement);
     listElement.appendChild(newListElement);
+
     inputElement.value = '';
 }
