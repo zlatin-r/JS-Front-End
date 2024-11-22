4d3043fabfe689ccb54c636de4892fb298bb777e
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-22 14:29:11 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/02. Delete-From-Table/delete-from-table.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/02. Delete-From-Table/delete-from-table.js
@@ -1,42 +1,43 @@
-// function deleteByEmail() {
-//     const inputElement = document.querySelector('input[name="email"]');
-//     const allEmailElements = document.querySelectorAll('tbody tr td:nth-of-type(2)');
-//     const resultElement = document.querySelector('#result');
-//     let flagFound = false;
-//
-//     allEmailElements.forEach(el => {
-//         if (el.textContent === inputElement.value) {
-//             el.parentElement.remove();
-//             flagFound = true;
-//         }
-//     });
-//
-//     if (flagFound) {
-//         resultElement.textContent = 'Deleted.';
-//     } else {
-//         resultElement.textContent = 'Not found.';
-//     }
-//
-//     inputElement.value = '';
-// }
-
 function deleteByEmail() {
-    const inputEl = document.querySelector('input[name="email"]');
-    const searchStr = inputEl.value.toLowerCase();
-    const people = document.querySelectorAll('table tbody tr td:nth-child(2)');
-    const resultEl = document.querySelector('#result');
+    const inputElement = document.querySelector('input[name="email"]');
+    const allEmailElements = document.querySelectorAll('tbody tr td:nth-of-type(2)');
+    const resultElement = document.querySelector('#result');
+    let flagFound = false;
 
-    if (searchStr === '') return;
-
-    const [matchedEl] = [...people].filter(person => {
-        return person.textContent.toLowerCase().includes(searchStr);
+    allEmailElements.forEach(el => {
+        if (el.textContent === inputElement.value) {
+            el.parentElement.remove();
+            flagFound = true;
+        }
     });
 
-    if (matchedEl) {
-        matchedEl.parentElement.remove();
-        resultEl.textContent = 'Deleted.';
+    if (flagFound) {
+        resultElement.textContent = 'Deleted.';
     } else {
-        resultEl.textContent = 'Not found.';
+        resultElement.textContent = 'Not found.';
     }
-    inputEl.value = '';
+
+    inputElement.value = '';
 }
+
+
+// function deleteByEmail() {
+//     const inputEl = document.querySelector('input[name="email"]');
+//     const searchStr = inputEl.value.toLowerCase();
+//     const people = document.querySelectorAll('table tbody tr td:nth-child(2)');
+//     const resultEl = document.querySelector('#result');
+//
+//     if (searchStr === '') return;
+//
+//     const [matchedEl] = [...people].filter(person => {
+//         return person.textContent.toLowerCase().includes(searchStr);
+//     });
+//
+//     if (matchedEl) {
+//         matchedEl.parentElement.remove();
+//         resultEl.textContent = 'Deleted.';
+//     } else {
+//         resultEl.textContent = 'Not found.';
+//     }
+//     inputEl.value = '';
+// }
