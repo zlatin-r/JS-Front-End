8caec7572bd3858b09950e2133d0e36c0c5d959b
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-22 14:28:50 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/02. Delete-From-Table/delete-from-table.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/02. Delete-From-Table/delete-from-table.js
@@ -1,21 +1,42 @@
-function deleteByEmail() {
-    const inputElement = document.querySelector('input[name="email"]');
-    const allEmailElements = document.querySelectorAll('tbody tr td:nth-of-type(2)');
-    const resultElement = document.querySelector('#result');
-    let flagFound = false;
+// function deleteByEmail() {
+//     const inputElement = document.querySelector('input[name="email"]');
+//     const allEmailElements = document.querySelectorAll('tbody tr td:nth-of-type(2)');
+//     const resultElement = document.querySelector('#result');
+//     let flagFound = false;
+//
+//     allEmailElements.forEach(el => {
+//         if (el.textContent === inputElement.value) {
+//             el.parentElement.remove();
+//             flagFound = true;
+//         }
+//     });
+//
+//     if (flagFound) {
+//         resultElement.textContent = 'Deleted.';
+//     } else {
+//         resultElement.textContent = 'Not found.';
+//     }
+//
+//     inputElement.value = '';
+// }
 
-    allEmailElements.forEach(el => {
-        if (el.textContent === inputElement.value) {
-            el.parentElement.remove();
-            flagFound = true;
-        }
+function deleteByEmail() {
+    const inputEl = document.querySelector('input[name="email"]');
+    const searchStr = inputEl.value.toLowerCase();
+    const people = document.querySelectorAll('table tbody tr td:nth-child(2)');
+    const resultEl = document.querySelector('#result');
+
+    if (searchStr === '') return;
+
+    const [matchedEl] = [...people].filter(person => {
+        return person.textContent.toLowerCase().includes(searchStr);
     });
 
-    if (flagFound) {
-        resultElement.textContent = 'Deleted.';
+    if (matchedEl) {
+        matchedEl.parentElement.remove();
+        resultEl.textContent = 'Deleted.';
     } else {
-        resultElement.textContent = 'Not found.';
+        resultEl.textContent = 'Not found.';
     }
-
-    inputElement.value = '';
+    inputEl.value = '';
 }
