17e4a790803af6c04a2dfd764838b7604c03d52e
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-22 14:31:34 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/01. List-Of-Items/list-of-items.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/01. List-Of-Items/list-of-items.js
@@ -8,3 +8,13 @@ function addItem() {
     listElement.appendChild(newListElement);
     inputElement.value = '';
 }
+
+
+// function addItem() {
+//     let text = document.getElementById('newItemText').value;
+//     let li = document.createElement("li");
+//     li.appendChild(document.createTextNode(text));
+//     document.getElementById("items").appendChild(li);
+//
+//     document.getElementById('newItemText').value = '';
+// }
