425183c1bb5f3e02f0fdab91a7e4914a2520e76e
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-24 13:05:28 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
@@ -3,18 +3,35 @@ document.addEventListener('DOMContentLoaded', solve);
 function solve() {
     const textAreaElement = document.querySelector('textarea');
     const addButtons = document.querySelectorAll('.add-product');
-    let totalPrice = 0;
+    const checkOutButton = document.querySelector('.checkout');
+    const allButtons = document.querySelectorAll('button');
+    const boughtProducts = {};
 
     addButtons.forEach((button) => {
-        button.addEventListener('click', () => {
+        button.addEventListener('click', (e) => {
 
-            const productPrice = button.closest('.product')
-                .querySelector('.product-line-price').textContent;
-            const productName = button.closest('.product')
-                .querySelector('.product-title').textContent;
-            totalPrice += parseFloat(productPrice);
+            const productElement = button.parentElement.parentElement;
+            const productPrice = productElement.querySelector('.product-line-price').textContent;
+            const productName = productElement.querySelector('.product-title').textContent;
+
+            if (boughtProducts.hasOwnProperty(productName)) {
+                boughtProducts[productName] += parseFloat(productPrice);
+            } else {
+                boughtProducts[productName] = parseFloat(productPrice);
+            }
 
             textAreaElement.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
         });
+    });
+
+    checkOutButton.addEventListener('click', (e) => {
+        const totalPrice = Object.values(boughtProducts).reduce((sum, price) => sum + price, 0);
+        const productsList = Object.keys(boughtProducts).join(', ');
+
+        allButtons.forEach((button) => {
+            button.disabled = true;
+        });
+
+        textAreaElement.textContent += `You bought ${productsList} for ${totalPrice.toFixed(2)}.`;
     });
 }
