732eaf94c43e752c50c6ea3926d69113befa2628
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-24 12:01:24 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
+    const textAreaElement = document.querySelector('textarea');
+    const allButtonElements = document.querySelectorAll('button');
 
+    allButtonElements.addEventListener('click', e => {
+        const price = e.target.parentElement.lastChild.textContent;
+        console.log(price);
+    });
 }
