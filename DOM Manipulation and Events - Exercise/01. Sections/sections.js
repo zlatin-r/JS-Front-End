96e33287559099d8062b9fcaaa2ec80dddf03784
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-24 14:43:00 +0100

    Initial Commit

--- DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   const inputStrings = document.querySelector('input[type="text"]').value;
-   const arrStrings = inputStrings.split(', ');
+    const inputStrings = document.querySelector('input[type="text"]').value;
+    const arrStrings = inputStrings.split(', ');
+    const contendElement = document.querySelector('#contend');
+    const generateElement = document.querySelector('input[type="submit"]');
 
-   debugger
-   arrStrings.forEach((str) => {
-       const newDiv = document.createElement('div');
-       const newParagraph = document.createElement('p');
-       newParagraph.textContent = str;
-
-       newDiv.appendChild(newParagraph);
-   });
+    generateElement.addEventListener('click', (e) => {
+        arrStrings.forEach((str) => {
+            const newDiv = document.createElement('div');
+            const newParagraph = document.createElement('p');
+            newParagraph.textContent = str;
+            console.log(newParagraph);
+            newDiv.appendChild(newParagraph);
+            contendElement.appendChild(newDiv);
+        });
+    })
 }
