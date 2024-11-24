3493e93abe720d6f03aa09af88734b851893e516
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-24 12:26:22 +0100

    Solution

--- DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
+++ DOM Manipulation and Events - Lab/DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
@@ -2,10 +2,19 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     const textAreaElement = document.querySelector('textarea');
-    const allButtonElements = document.querySelectorAll('button');
+    const addButtons = document.querySelectorAll('.add-product');
+    let totalPrice = 0;
 
-    allButtonElements.addEventListener('click', e => {
-        const price = e.target.parentElement.lastChild.textContent;
-        console.log(price);
+    addButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+
+            const productPrice = button.closest('.product')
+                .querySelector('.product-line-price').textContent;
+            const productName = button.closest('.product')
+                .querySelector('.product-title').textContent;
+            totalPrice += parseFloat(productPrice);
+
+            textAreaElement.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
+        });
     });
 }
