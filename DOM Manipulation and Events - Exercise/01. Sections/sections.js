c1a308e1987a1d5065e626fe6710ef0631eeaffb
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-24 14:35:24 +0100

    Initial Commit

--- DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   const inputStrings = document.querySelector('input[type="text"]').value;
+   const arrStrings = inputStrings.split(', ');
+
+   debugger
+   arrStrings.forEach((str) => {
+       const newDiv = document.createElement('div');
+       const newParagraph = document.createElement('p');
+       newParagraph.textContent = str;
+
+       newDiv.appendChild(newParagraph);
+   });
 }
