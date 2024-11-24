e0f2db856fef3aaaae6ebd1cb2d08309265294b8
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-24 15:18:23 +0100

    Initial Commit

--- DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,19 +1,45 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
+    const contendElement = document.querySelector('#content');
+    const generateElement = document.querySelector('input[type="submit"]');
     const inputStrings = document.querySelector('input[type="text"]').value;
     const arrStrings = inputStrings.split(', ');
-    const contendElement = document.querySelector('#contend');
-    const generateElement = document.querySelector('input[type="submit"]');
 
-    generateElement.addEventListener('click', (e) => {
-        arrStrings.forEach((str) => {
-            const newDiv = document.createElement('div');
-            const newParagraph = document.createElement('p');
-            newParagraph.textContent = str;
-            console.log(newParagraph);
-            newDiv.appendChild(newParagraph);
-            contendElement.appendChild(newDiv);
-        });
+    arrStrings.forEach((word) => {
+        let currentParagraph = document.createElement("p");
+        let currentDiv = document.createElement("div");
+
+        currentParagraph.textContent = word;
+        currentParagraph.style.display = 'none';
+
+        currentDiv.appendChild(currentParagraph);
     })
 }
+
+
+// function solve() {
+//
+//     const contendElement = document.querySelector('#content');
+//     const generateElement = document.querySelector('input[type="submit"]');
+//
+//     generateElement.addEventListener('click', (e) => {
+//         e.preventDefault();
+//
+//         const inputStrings = document.querySelector('input[type="text"]').value;
+//         const arrStrings = inputStrings.split(', ');
+//
+//
+//         arrStrings.forEach((str) => {
+//
+//             const newDiv = document.createElement('div');
+//             const newParagraph = document.createElement('p');
+//
+//             newParagraph.textContent = str;
+//             newDiv.appendChild(newParagraph);
+//             contendElement.appendChild(newDiv);
+//         });
+//     });
+// }
+
+
