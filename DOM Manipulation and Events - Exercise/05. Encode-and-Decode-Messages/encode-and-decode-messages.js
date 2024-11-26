062249d52053d9e4d9bcc8a291568abaca0159fa
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-26 14:45:58 +0100

    Solution

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const encodeAndSendButton = document.querySelector('#encode button');
+    const decodeAndReadButton = document.querySelector('#decode textarea');
+    const encodedMessageEl = document.querySelector('#encode textarea');
+
+    encodeAndSendButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        // console.log(decodeMessage(encodedMessageEl.value));
+    });
+
+    function decodeMessage(encodedMessage) {
+        return Array.from(encodedMessage)
+            .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+            .join('');
+    }
+    function encodeMessage(decodedMessage) {
+        return Array.from(decodedMessage)
+            .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+            .join('');
+    }
 }
