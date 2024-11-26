6ac4bdf95ee8b67d6084342c73f4d4b2791501c9
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-26 14:52:32 +0100

    Solution

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -4,12 +4,19 @@ function solve() {
     const encodeAndSendButton = document.querySelector('#encode button');
     const decodeAndReadButton = document.querySelector('#decode textarea');
     const encodedMessageEl = document.querySelector('#encode textarea');
+    const decodedMessageEL = document.querySelector('#decode textarea');
 
     encodeAndSendButton.addEventListener('click', (e) => {
         e.preventDefault();
-
+        decodedMessageEL.value = encodeMessage(encodedMessageEl.value);
+        encodedMessageEl.value = '';
         // console.log(decodeMessage(encodedMessageEl.value));
     });
+
+    decodeAndReadButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        decodedMessageEL.value = decodeMessage(decodedMessageEL.value);
+    })
 
     function decodeMessage(encodedMessage) {
         return Array.from(encodedMessage)
@@ -18,7 +25,7 @@ function solve() {
     }
     function encodeMessage(decodedMessage) {
         return Array.from(decodedMessage)
-            .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+            .map(char => String.fromCharCode(char.charCodeAt(0) - 1))
             .join('');
     }
 }
