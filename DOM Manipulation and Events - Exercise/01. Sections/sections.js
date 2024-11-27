15dd0a716cf4cac3aea73994641df0d61e306483
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-27 14:56:51 +0100

    Solution

--- DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,64 +1,60 @@
 document.addEventListener('DOMContentLoaded', solve);
 
-// function solve() {
-//
-//     const contendElement = document.querySelector('#content');
-//     const generateElement = document.querySelector('input[type="submit"]');
-//
-//     generateElement.addEventListener('click', (e) => {
-//         generateElement.disabled = true;
-//         e.preventDefault();
-//
-//         const inputStrings = document.querySelector('input[type="text"]').value;
-//
-//         if (inputStrings.trim() !== "") {
-//             const arrStrings = inputStrings.split(', ');
-//             arrStrings.forEach((str) => {
-//
-//                 const newDiv = document.createElement('div');
-//                 const newParagraph = document.createElement('p');
-//
-//                 newParagraph.textContent = str;
-//                 newParagraph.style.display = 'flex';
-//                 newParagraph.style.color = 'transparent';
-//                 newParagraph.style.alignItems = 'center';
-//                 newParagraph.style.justifyContent = 'center';
-//                 newParagraph.style.width = '100%';
-//                 newParagraph.style.height = '100%';
-//
-//                 newDiv.appendChild(newParagraph);
-//                 newParagraph.addEventListener('click', makeTextVisible);
-//                 contendElement.appendChild(newDiv);
-//             });
-//         }
-//     });
-//
-//     function makeTextVisible(event) {
-//         event.target.style.color = "white";
-//     }
-// }
-
-//---------------------------------------------------------------------------------------------------------------------
-
 function solve() {
-    const formEl = document.querySelector('#task-input');
-    const contentEl = document.querySelector('#content');
 
-    formEl.addEventListener('submit', (e) => {
+    const contendElement = document.querySelector('#content');
+    const generateElement = document.querySelector('input[type="submit"]');
+
+    generateElement.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const sections = formEl.querySelector('input[type="text"]').value.split(', ');
-        sections.forEach(el => {
+        const inputStrings = document.querySelector('input[type="text"]').value.split(', ');
 
-            const newDivEl = document.createElement('div');
-            const newPEl = document.createElement('p');
+        inputStrings.forEach((str) => {
 
-            newPEl.textContent = el;
+            const newDiv = document.createElement('div');
+            const newParagraph = document.createElement('p');
 
-            newDivEl.appendChild(newPEl);
+            newParagraph.textContent = str;
+            newParagraph.style.display = 'none';
 
-            contentEl.appendChild(newDivEl);
+            newDiv.appendChild(newParagraph);
+            newDiv.addEventListener('click', (e) => {
+                e.target.querySelector('p').style.display = 'block';
+            });
 
-        })
-    })
+            contendElement.appendChild(newDiv);
+        });
+    });
 }
+
+
+//---------------------------------------------------------------------------------------------------------------------
+// Lector's Solution
+//
+// function solve() {
+//     const formEl = document.querySelector('#task-input');
+//     const contentEl = document.querySelector('#content');
+//
+//     formEl.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//
+//         const sections = formEl.querySelector('input[type="text"]').value.split(', ');
+//         sections.forEach(el => {
+//
+//             const newDivEl = document.createElement('div');
+//             const newPEl = document.createElement('p');
+//
+//             newPEl.textContent = el;
+//
+//             newPEl.style.display = 'none';
+//
+//             newDivEl.appendChild(newPEl);
+//             newDivEl.addEventListener('click', (e) => {
+//                 e.target.querySelector('p').style.display = 'block';
+//             });
+//
+//             contentEl.appendChild(newDivEl);
+//         });
+//     });
+// }
