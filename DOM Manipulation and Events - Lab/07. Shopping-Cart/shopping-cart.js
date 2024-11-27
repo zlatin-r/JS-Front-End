e0b81db70b13004606b93354edd28e0f0d000f75
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-27 14:30:56 +0100

    Solution

--- DOM Manipulation and Events - Lab/07. Shopping-Cart/shopping-cart.js
+++ DOM Manipulation and Events - Lab/07. Shopping-Cart/shopping-cart.js
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', solve);
 // }
 
 //-------------------------------------------------------------------------------------------------------------------
+// Lector's Solution
 
 function solve() {
     const resultEL = document.querySelector('textarea[disabled]');
@@ -44,7 +45,7 @@ function solve() {
 
     const products = {};
 
-    prodCatalogEl.addEventListener('change', (e) => {
+    prodCatalogEl.addEventListener('click', (e) => {
         if (e.target.nodeName !== 'BUTTON') return;
 
         switch (e.target.getAttribute('class')) {
@@ -54,7 +55,7 @@ function solve() {
                 const name = productEl.querySelector('.product-title').textContent;
                 const price = Number(productEl.querySelector('.product-line-price').textContent);
 
-                resultEL.value += `Added ${name} for ${price.toFixed(2)} to the cart.`;
+                resultEL.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
 
                 products[name] ??= 0;
                 products[name] += price;
@@ -66,11 +67,10 @@ function solve() {
                 const productNames = Object.keys(products);
                 const totalPrice = productNames.reduce((price, name) => price + products[name], 0)
 
-                resultEL.value += `You brought ${productNames.join(', ')} for ${totalPrice.toFixed(2)}.`;
+                resultEL.value += `You bought ${productNames.join(', ')} for ${totalPrice.toFixed(2)}.`;
 
-                prodCatalogEl.querySelectorAll('button').forEach(el => {
-                    el.setAttribute('disabled', 'disabled');
-                });
+                prodCatalogEl.querySelectorAll('button').forEach(el =>
+                    el.setAttribute('disabled', 'disabled'));
 
                 break;
         }
