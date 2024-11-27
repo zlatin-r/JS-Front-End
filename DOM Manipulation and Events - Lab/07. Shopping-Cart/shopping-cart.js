7884c8c075c205d4773a1e8e4d0a3b0128368dbc
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-27 14:00:00 +0100

    Initial Commit

--- DOM Manipulation and Events - Lab/07. Shopping-Cart/shopping-cart.js
+++ DOM Manipulation and Events - Lab/07. Shopping-Cart/shopping-cart.js
@@ -1,37 +1,60 @@
 document.addEventListener('DOMContentLoaded', solve);
 
+// function solve() {
+//     const textAreaElement = document.querySelector('textarea');
+//     const addButtons = document.querySelectorAll('.add-product');
+//     const checkOutButton = document.querySelector('.checkout');
+//     const allButtons = document.querySelectorAll('button');
+//     const boughtProducts = {};
+//
+//     addButtons.forEach((button) => {
+//         button.addEventListener('click', (e) => {
+//
+//             const productElement = button.parentElement.parentElement;
+//             const productPrice = productElement.querySelector('.product-line-price').textContent;
+//             const productName = productElement.querySelector('.product-title').textContent;
+//
+//             if (boughtProducts.hasOwnProperty(productName)) {
+//                 boughtProducts[productName] += parseFloat(productPrice);
+//             } else {
+//                 boughtProducts[productName] = parseFloat(productPrice);
+//             }
+//
+//             textAreaElement.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
+//         });
+//     });
+//
+//     checkOutButton.addEventListener('click', (e) => {
+//         const totalPrice = Object.values(boughtProducts).reduce((sum, price) => sum + price, 0);
+//         const productsList = Object.keys(boughtProducts).join(', ');
+//
+//         allButtons.forEach((button) => {
+//             button.disabled = true;
+//         });
+//
+//         textAreaElement.textContent += `You bought ${productsList} for ${totalPrice.toFixed(2)}.`;
+//     });
+// }
+
+//-------------------------------------------------------------------------------------------------------------------
+
 function solve() {
-    const textAreaElement = document.querySelector('textarea');
-    const addButtons = document.querySelectorAll('.add-product');
-    const checkOutButton = document.querySelector('.checkout');
-    const allButtons = document.querySelectorAll('button');
-    const boughtProducts = {};
+    const resultEL = document.querySelector('textarea[disabled]');
+    const checkoutBtnEl = document.querySelector('button.checkout');
+    const prodCatalogEl = document.querySelector('.shopping-cart');
 
-    addButtons.forEach((button) => {
-        button.addEventListener('click', (e) => {
+    const products = {};
 
-            const productElement = button.parentElement.parentElement;
-            const productPrice = productElement.querySelector('.product-line-price').textContent;
-            const productName = productElement.querySelector('.product-title').textContent;
+    prodCatalogEl.addEventListener('change', (e) => {
+        if (e.target.nodeName !== 'BUTTON') return;
 
-            if (boughtProducts.hasOwnProperty(productName)) {
-                boughtProducts[productName] += parseFloat(productPrice);
-            } else {
-                boughtProducts[productName] = parseFloat(productPrice);
-            }
-
-            textAreaElement.textContent += `Added ${productName} for ${productPrice} to the cart.\n`;
-        });
-    });
-
-    checkOutButton.addEventListener('click', (e) => {
-        const totalPrice = Object.values(boughtProducts).reduce((sum, price) => sum + price, 0);
-        const productsList = Object.keys(boughtProducts).join(', ');
-
-        allButtons.forEach((button) => {
-            button.disabled = true;
-        });
-
-        textAreaElement.textContent += `You bought ${productsList} for ${totalPrice.toFixed(2)}.`;
-    });
+        switch (e.target.getAttribute('class')) {
+            case 'add-product':
+                console.log(checkoutBtnEl);
+                break;
+            case 'checkout':
+                console.log(checkoutBtnEl);
+                break;
+        }
+    })
 }
