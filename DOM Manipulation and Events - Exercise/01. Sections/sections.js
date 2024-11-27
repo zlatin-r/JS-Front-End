abe3465a96c28001cf89eaf30139d1f757c6cbab
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-27 14:41:59 +0100

    Solution

--- DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,39 +1,64 @@
 document.addEventListener('DOMContentLoaded', solve);
 
+// function solve() {
+//
+//     const contendElement = document.querySelector('#content');
+//     const generateElement = document.querySelector('input[type="submit"]');
+//
+//     generateElement.addEventListener('click', (e) => {
+//         generateElement.disabled = true;
+//         e.preventDefault();
+//
+//         const inputStrings = document.querySelector('input[type="text"]').value;
+//
+//         if (inputStrings.trim() !== "") {
+//             const arrStrings = inputStrings.split(', ');
+//             arrStrings.forEach((str) => {
+//
+//                 const newDiv = document.createElement('div');
+//                 const newParagraph = document.createElement('p');
+//
+//                 newParagraph.textContent = str;
+//                 newParagraph.style.display = 'flex';
+//                 newParagraph.style.color = 'transparent';
+//                 newParagraph.style.alignItems = 'center';
+//                 newParagraph.style.justifyContent = 'center';
+//                 newParagraph.style.width = '100%';
+//                 newParagraph.style.height = '100%';
+//
+//                 newDiv.appendChild(newParagraph);
+//                 newParagraph.addEventListener('click', makeTextVisible);
+//                 contendElement.appendChild(newDiv);
+//             });
+//         }
+//     });
+//
+//     function makeTextVisible(event) {
+//         event.target.style.color = "white";
+//     }
+// }
+
+//---------------------------------------------------------------------------------------------------------------------
+
 function solve() {
+    const formEl = document.querySelector('#task-input');
+    const contentEl = document.querySelector('#content');
 
-    const contendElement = document.querySelector('#content');
-    const generateElement = document.querySelector('input[type="submit"]');
-
-    generateElement.addEventListener('click', (e) => {
-        generateElement.disabled = true;
+    formEl.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const inputStrings = document.querySelector('input[type="text"]').value;
+        const sections = formEl.querySelector('input[type="text"]').value.split(', ');
+        sections.forEach(el => {
 
-        if (inputStrings.trim() !== "") {
-            const arrStrings = inputStrings.split(', ');
-            arrStrings.forEach((str) => {
+            const newDivEl = document.createElement('div');
+            const newPEl = document.createElement('p');
 
-                const newDiv = document.createElement('div');
-                const newParagraph = document.createElement('p');
+            newPEl.textContent = el;
 
-                newParagraph.textContent = str;
-                newParagraph.style.display = 'flex';
-                newParagraph.style.color = 'transparent';
-                newParagraph.style.alignItems = 'center';
-                newParagraph.style.justifyContent = 'center';
-                newParagraph.style.width = '100%';
-                newParagraph.style.height = '100%';
+            newDivEl.appendChild(newPEl);
 
-                newDiv.appendChild(newParagraph);
-                newParagraph.addEventListener('click', makeTextVisible);
-                contendElement.appendChild(newDiv);
-            });
-        }
-    });
+            contentEl.appendChild(newDivEl);
 
-    function makeTextVisible(event) {
-        event.target.style.color = "white";
-    }
+        })
+    })
 }
