a45d00ec772140ad8315ae6f89322c9d12377f5d
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-27 16:19:32 +0100

    Solution

--- DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
+++ DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
@@ -1,88 +1,95 @@
 document.addEventListener('DOMContentLoaded', solve);
 
+function solve() {
+    const inputTextEl = document.querySelector('#input textarea').value;
+    console.log(JSON.parse(inputTextEl));
+}
+
+
+
 //---------------------------------------------------------------------------------------------------
 
-function solve() {
-    const inputDataEl = document.querySelector('#input textarea');
-    const matches = inputDataEl.value.match(/{.*?}/g);
-    const tableBodyEl = document.querySelector('table tbody');
-    const generateEl = document.querySelector('input[value="Generate"]');
-    const buyEl = document.querySelector('input[value="Buy"]');
-
-    const broughtFurniture = [];
-    let totalPrice = 0;
-    let totalDecFactor = 0;
-
-    const resultTextArea = document.querySelector('#shop textarea')
-
-    generateEl.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        matches.forEach(match => {
-            const obj = JSON.parse(match);
-
-            const newRow = document.createElement('tr');
-
-            const newTdImg = document.createElement('td');
-            const newImg = document.createElement('img');
-
-            newImg.setAttribute('src', obj.img);
-            newTdImg.appendChild(newImg);
-            newRow.appendChild(newTdImg);
-
-            const newTdName = document.createElement('td');
-            const newParagraphForName = document.createElement('p');
-            newParagraphForName.textContent = obj.name;
-
-            newTdName.appendChild(newParagraphForName);
-            newRow.appendChild(newTdName);
-
-            const newTdPrice = document.createElement('td');
-            const newParagraphForPrice = document.createElement('p');
-            newParagraphForPrice.textContent = obj.price;
-
-            newTdPrice.appendChild(newParagraphForPrice);
-            newRow.appendChild(newTdPrice);
-
-            const newTdDecFactor = document.createElement('td');
-            const newParagraphForDecFactor = document.createElement('p');
-            newParagraphForDecFactor.textContent = obj.decFactor;
-
-            newTdDecFactor.appendChild(newParagraphForDecFactor);
-            newRow.appendChild(newTdDecFactor);
-
-            const newTdCheckbox = document.createElement('td');
-            const newInput = document.createElement('input');
-            newInput.type = 'checkbox';
-
-            newTdCheckbox.appendChild(newInput);
-            newRow.appendChild(newTdCheckbox);
-
-            tableBodyEl.appendChild(newRow);
-        });
-    });
-
-    buyEl.addEventListener('click', (e) => {
-        e.preventDefault();
-        const allRows = document.querySelectorAll('tbody tr');
-
-        allRows.forEach(row => {
-            const checkbox = row.querySelector('input[type="checkbox"]');
-
-            if (checkbox.checked && checkbox) {
-                broughtFurniture.push(row.querySelector('td:nth-child(2) p').textContent);
-                totalPrice += parseFloat(row.querySelector('td:nth-child(3) p').textContent);
-                totalDecFactor += parseFloat(row.querySelector('td:nth-child(4) p').textContent);
-            }
-        });
-
-        const avgDecFactor = (totalDecFactor / broughtFurniture.length).toFixed(2);
-
-        resultTextArea.textContent += `Bought furniture: ${broughtFurniture.join(', ')}\n`;
-        resultTextArea.textContent += `Total price: ${totalPrice}\n`;
-        resultTextArea.textContent += `Average decoration factor: ${avgDecFactor}`;
-    });
-}
+// function solve() {
+//     const inputDataEl = document.querySelector('#input textarea');
+//     const matches = inputDataEl.value.match(/{.*?}/g);
+//     const tableBodyEl = document.querySelector('table tbody');
+//     const generateEl = document.querySelector('input[value="Generate"]');
+//     const buyEl = document.querySelector('input[value="Buy"]');
+//
+//     const broughtFurniture = [];
+//     let totalPrice = 0;
+//     let totalDecFactor = 0;
+//
+//     const resultTextArea = document.querySelector('#shop textarea')
+//
+//     generateEl.addEventListener('click', (e) => {
+//         e.preventDefault();
+//
+//         matches.forEach(match => {
+//             const obj = JSON.parse(match);
+//
+//             const newRow = document.createElement('tr');
+//
+//             const newTdImg = document.createElement('td');
+//             const newImg = document.createElement('img');
+//
+//             newImg.setAttribute('src', obj.img);
+//             newTdImg.appendChild(newImg);
+//             newRow.appendChild(newTdImg);
+//
+//             const newTdName = document.createElement('td');
+//             const newParagraphForName = document.createElement('p');
+//             newParagraphForName.textContent = obj.name;
+//
+//             newTdName.appendChild(newParagraphForName);
+//             newRow.appendChild(newTdName);
+//
+//             const newTdPrice = document.createElement('td');
+//             const newParagraphForPrice = document.createElement('p');
+//             newParagraphForPrice.textContent = obj.price;
+//
+//             newTdPrice.appendChild(newParagraphForPrice);
+//             newRow.appendChild(newTdPrice);
+//
+//             const newTdDecFactor = document.createElement('td');
+//             const newParagraphForDecFactor = document.createElement('p');
+//             newParagraphForDecFactor.textContent = obj.decFactor;
+//
+//             newTdDecFactor.appendChild(newParagraphForDecFactor);
+//             newRow.appendChild(newTdDecFactor);
+//
+//             const newTdCheckbox = document.createElement('td');
+//             const newInput = document.createElement('input');
+//             newInput.type = 'checkbox';
+//
+//             newTdCheckbox.appendChild(newInput);
+//             newRow.appendChild(newTdCheckbox);
+//
+//             tableBodyEl.appendChild(newRow);
+//         });
+//     });
+//
+//     buyEl.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         const allRows = document.querySelectorAll('tbody tr');
+//
+//         allRows.forEach(row => {
+//             const checkbox = row.querySelector('input[type="checkbox"]');
+//
+//             if (checkbox.checked && checkbox) {
+//                 broughtFurniture.push(row.querySelector('td:nth-child(2) p').textContent);
+//                 totalPrice += parseFloat(row.querySelector('td:nth-child(3) p').textContent);
+//                 totalDecFactor += parseFloat(row.querySelector('td:nth-child(4) p').textContent);
+//             }
+//         });
+//
+//         const avgDecFactor = (totalDecFactor / broughtFurniture.length).toFixed(2);
+//
+//         resultTextArea.textContent += `Bought furniture: ${broughtFurniture.join(', ')}\n`;
+//         resultTextArea.textContent += `Total price: ${totalPrice}\n`;
+//         resultTextArea.textContent += `Average decoration factor: ${avgDecFactor}`;
+//     });
+// }
 
 //---------------------------------------------------------------------------------------------------
 // Chat GPT
