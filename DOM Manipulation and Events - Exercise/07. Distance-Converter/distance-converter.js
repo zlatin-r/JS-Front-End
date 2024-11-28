0dcdd8f0c791b694385900bfb53b40f7f4a0f190
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 13:48:02 +0100

    Solution

--- DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
+++ DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     const convertBtnEl = document.querySelector('#convert');
+    const values = {mm: 1, cm: 10, in: 25.4, ft: 304.8, yrd: 914.4, m: 1000, km: 1000000, mi: 1609344}
 
     convertBtnEl.addEventListener('click', (e) => {
         e.preventDefault();
