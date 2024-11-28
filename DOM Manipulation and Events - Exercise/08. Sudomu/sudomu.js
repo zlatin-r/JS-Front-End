888b6806656e8b3b01eef07f76c8c2c7974b4951
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 14:56:00 +0100

    Solution

--- DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
+++ DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
+    document.querySelector('.buttons').addEventListener('click', (e) => {
+        e.preventDefault();
 
+        if (e.target.value === 'Quick Check') {
+            const allRows = document.querySelectorAll('tr');
+            const tableData = [];
+
+            allRows.forEach((row, index) => {
+                const cells = row.querySelectorAll('td');
+                const rowData = [];
+
+                cells.forEach((cell, colIndex) => {
+                    const input = cell.querySelector('input');
+                    if (input) {
+                        rowData.push(input.value);
+                    }
+                });
+                tableData.push(rowData);
+            });
+            console.log(tableData);
+        } else if (e.target.value === 'Clear') {
+            console.log('Clear');
+        }
+    });
 }
