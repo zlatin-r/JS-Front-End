e2efda100f7c10eb1ca312c425bc7c51d7b62aa7
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 14:05:36 +0100

    Solution

--- DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
+++ DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
@@ -2,15 +2,32 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     const convertBtnEl = document.querySelector('#convert');
-    const values = {mm: 1, cm: 10, in: 25.4, ft: 304.8, yrd: 914.4, m: 1000, km: 1000000, mi: 1609344}
+    const values = {
+        mm: 1,
+        cm: 10,
+        in: 25.4,
+        ft: 304.8,
+        yrd: 914.4,
+        m: 1000,
+        km: 1000000,
+        mi: 1609344
+    }
 
     convertBtnEl.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const inputTypeEl = document.querySelector('#inputUnits');
-        const outputTypeEl = document.querySelector('#outputUnits');
+        const distanceValueEl = Number(document.querySelector('#inputDistance').value);
 
-        console.log(inputTypeEl.value, outputTypeEl.value);
+        if (distanceValueEl < 1) return;
+
+        const inputUnitEl = document.querySelector('#inputUnits');
+        const outputUnitEl = document.querySelector('#outputUnits');
+
+        const result = Number(values[inputUnitEl.value]) * distanceValueEl;
 
     });
+
+    function convertTo(millimeters, unit) {
+
+    }
 }
