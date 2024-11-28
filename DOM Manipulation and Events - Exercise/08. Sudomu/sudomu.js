9d574fff8f465cf4d77ebb6d88899aeb31f4190a
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 16:10:25 +0100

    Solution

--- DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
+++ DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
@@ -1,55 +1,73 @@
 document.addEventListener('DOMContentLoaded', solve);
 
-function solve() {
-    const resultMessageEl = document.querySelector('#check');
+// function solve() {
+//     const resultMessageEl = document.querySelector('#check');
+//
+//     document.querySelector('.buttons').addEventListener('click', (e) => {
+//         e.preventDefault();
+//
+//         if (e.target.value === 'Quick Check') {
+//             const allRows = document.querySelectorAll('tr');
+//             const allRowsData = [];
+//
+//             allRows.forEach((row) => {
+//                 const cells = row.querySelectorAll('td');
+//                 const rowData = [];
+//
+//                 cells.forEach((cell) => {
+//                     const input = cell.querySelector('input');
+//                     rowData.push(input.value);
+//                 });
+//                 allRowsData.push(rowData);
+//             });
+//
+//             const allColsData = [0, 1, 2].map(colIndex => {
+//                 const column = []
+//
+//                 allRowsData.forEach(row => {
+//                     column.push(row[colIndex]);
+//                 });
+//                 return column;
+//             });
+//
+//             if (areArraysUnique(allRowsData) && areArraysUnique(allColsData)) {
+//                 resultMessageEl.textContent = 'Success!';
+//             } else {
+//                 resultMessageEl.textContent = 'Keep trying ...';
+//             }
+//
+//         } else if (e.target.value === 'Clear') {
+//             resultMessageEl.textContent = '';
+//         }
+//     });
+//
+//     function areArraysUnique(arrays) {
+//         return arrays.every((array, index, self) => {
+//
+//             const isUniqueElements = new Set(array).size === array.length;
+//             const isUniqueArray = self.findIndex(a => JSON.stringify(a) === JSON.stringify(array)) === index;
+//
+//             return isUniqueElements && isUniqueArray;
+//         });
+//     }
+// }
 
-    document.querySelector('.buttons').addEventListener('click', (e) => {
+//---------------------------------------------------------------------------------------------------------------------
+
+//Lector's Solution
+
+function solve() {
+    const formEl = document.querySelector('#solutionCheck');
+    const outputEl = document.querySelector('#check');
+
+    formEl.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        if (e.target.value === 'Quick Check') {
-            const allRows = document.querySelectorAll('tr');
-            const allRowsData = [];
+        const rows= e.target.querySelectorAll('table tbody tr');
 
-            allRows.forEach((row) => {
-                const cells = row.querySelectorAll('td');
-                const rowData = [];
+        rows.forEach((row) => {
 
-                cells.forEach((cell) => {
-                    const input = cell.querySelector('input');
-                    if (input) {
-                        rowData.push(input.value);
-                    }
-                });
-                allRowsData.push(rowData);
-            });
-
-            const allColsData = [0, 1, 2].map(colIndex => {
-                const column = []
-                allRowsData.forEach(row => {
-                    column.push(row[colIndex]);
-                });
-                return column;
-            });
-
-            if (areArraysUnique(allRowsData) && areArraysUnique(allColsData)) {
-                resultMessageEl.textContent = 'Success!';
-            } else {
-                resultMessageEl.textContent = 'Keep trying ...';
-            }
-
-        } else if (e.target.value === 'Clear') {
-            document.querySelector('form').reset();
-            resultMessageEl.textContent = '';
-        }
-    });
-
-    function areArraysUnique(arrays) {
-        return arrays.every((array, index, self) => {
-
-            const isUniqueElements = new Set(array).size === array.length;
-            const isUniqueArray = self.findIndex(a => JSON.stringify(a) === JSON.stringify(array)) === index;
-
-            return isUniqueElements && isUniqueArray;
         });
-    }
+
+    });
 }
