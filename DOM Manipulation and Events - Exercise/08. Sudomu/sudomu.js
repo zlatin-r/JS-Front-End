0f4ca995fa3bcd5e13220d968181e59973fcb85a
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 16:34:52 +0100

    Solution

--- DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
+++ DOM Manipulation and Events - Exercise/08. Sudomu/sudomu.js
@@ -59,15 +59,46 @@ document.addEventListener('DOMContentLoaded', solve);
 function solve() {
     const formEl = document.querySelector('#solutionCheck');
     const outputEl = document.querySelector('#check');
+    let status = '';
+    let size = 3;
 
     formEl.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const rows= e.target.querySelectorAll('table tbody tr');
+        const rows = e.target.querySelectorAll('table tbody tr');
 
         rows.forEach((row) => {
+            const values = [...row.children].map(el => el.children[0].value);
+            const duplicates = values.filter((item, index) => values.indexOf(item) !== index);
 
+            if (duplicates.length > 0) {
+                status += '0';
+            } else {
+                status += '1';
+            }
         });
 
+        status += 'x';
+
+        for (let i = 1; i <= size; i++) {
+            const column = [...e.target.querySelectorAll(`tbody tr td:nth-child(${i})`)].map(el => el.children[0].value)
+            const duplicates = column.filter((item, index) => column.indexOf(item) !== index);
+
+            if (duplicates.length > 0) {
+                status += '0';
+            } else {
+                status += '1';
+            }
+        }
+
+        if (status === '111x111') {
+            outputEl.classList.remove('fail');
+            outputEl.classList.add('success');
+            outputEl.textContent = 'Success!';
+        } else {
+            outputEl.classList.add('fail');
+            outputEl.classList.remove('success');
+            outputEl.textContent = 'Keep trying ...';
+        }
     });
 }
