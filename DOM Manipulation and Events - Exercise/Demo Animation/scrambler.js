e2b3feb7af23ddde0cfeca83dfc469d416417086
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 17:41:11 +0100

    Solution

--- DOM Manipulation and Events - Exercise/Demo Animation/scrambler.js
+++ DOM Manipulation and Events - Exercise/Demo Animation/scrambler.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const textEl = document.querySelector('.text');
+    const defaultChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.?~';
+    const randomString = (length, chars = defaultChars) => {
+        return [...Array(length)]
+            .map(() => chars[Math.floor(Math.random() * chars.length)])
+            .join('')
+    }
 
+    const textEl = document.querySelector('.text');
     textEl.textContent.trim().split(' ').forEach(word => {
 
         const wordEl = document.createElement('span');
@@ -9,14 +15,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const spaceEl = document.createElement('span');
         spaceEl.classList.add('space');
 
-        word.split(' ').forEach(letter => {
+        word.split('').forEach(letter => {
             const letterEl = document.createElement('span');
             letterEl.classList.add('letter');
 
             const charactersEl = document.createElement('span');
-            charactersEl.classList.add('character');
+            charactersEl.classList.add('characters');
+            charactersEl.textContent = randomString(20) + letter.toLocaleUpperCase();
+
+            letterEl.append(charactersEl);
+            wordEl.append(letterEl);
         })
 
         textEl.append(wordEl, spaceEl)
+    })
+
+    document.querySelectorAll('.characters').forEach((el, i) => {
+        el.style.transitionDelay = (0.02 * i) + 's';
     })
 })
