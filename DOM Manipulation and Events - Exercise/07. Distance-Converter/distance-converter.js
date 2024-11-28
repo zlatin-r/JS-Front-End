4da726a8220fee84f6c7b2ea78bb6efda1b92bf1
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-28 13:38:57 +0100

    Solution

--- DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
+++ DOM Manipulation and Events - Exercise/07. Distance-Converter/distance-converter.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const convertBtnEl = document.querySelector('#convert');
+
+    convertBtnEl.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const inputTypeEl = document.querySelector('#inputUnits');
+        const outputTypeEl = document.querySelector('#outputUnits');
+
+        console.log(inputTypeEl.value, outputTypeEl.value);
+
+    });
 }
