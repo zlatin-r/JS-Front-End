ee6b36afffbbe03145954045fbca9a5231f27ce1
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-29 08:37:16 +0100

    Solution

--- DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
+++ DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
@@ -41,3 +41,46 @@ function solve() {
 }
 
 
+//---------------------------------------------------------------------------------------------------------------------
+// Solution from GitHub - GeorgiDN / Georgi Nikolov
+//
+// function solve() {
+//     const rightAnswers = [
+//         "onclick",
+//         "JSON.stringify()",
+//         "A programming API for HTML and XML documents"
+//     ];
+//     const sectionsEl = document.querySelectorAll("section");
+//     const result = document.querySelector("#results");
+//     let countRightAnswers = 0;
+//     let currentSectionIndex = 0;
+//
+//     sectionsEl.forEach((section, index) => {
+//         const listAnswers = section.querySelectorAll("li");
+//         listAnswers.forEach(answer => {
+//             answer.addEventListener("click", () => {
+//                 if (rightAnswers.includes(answer.textContent)) {
+//                     countRightAnswers++;
+//                 }
+//
+//                 sectionsEl[index].classList.add("hidden");
+//                 currentSectionIndex++;
+//
+//                 if (currentSectionIndex < sectionsEl.length) {
+//                     sectionsEl[currentSectionIndex].classList.remove("hidden");
+//                 } else {
+//                     if (countRightAnswers === sectionsEl.length) {
+//                         result.textContent = "You are recognized as top JavaScript fan!";
+//                     } else if (countRightAnswers === 1) {
+//                         result.textContent = `You have ${countRightAnswers} right answer`;
+//                     } else {
+//                         result.textContent = `You have ${countRightAnswers} right answers`;
+//                     }
+//                     result.style.display = "block";
+//                 }
+//             });
+//         });
+//     });
+// }
+
+
