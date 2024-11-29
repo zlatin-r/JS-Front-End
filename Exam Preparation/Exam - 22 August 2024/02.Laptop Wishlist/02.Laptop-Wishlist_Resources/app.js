e282ced4c57f401288bd1f6aeaeb32b384f697c4
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-29 20:19:18 +0100

    Solution

--- Exam Preparation/Exam - 22 August 2024/02.Laptop Wishlist/02.Laptop-Wishlist_Resources/app.js
+++ Exam Preparation/Exam - 22 August 2024/02.Laptop Wishlist/02.Laptop-Wishlist_Resources/app.js
@@ -1,6 +1,31 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO...
-  }
+    const addLaptopFormInputEl = document.querySelectorAll('.laptop-info input');
+    const addBtnEl = document.querySelector('.laptop-info button');
+    const checkListEl = document.querySelector('#check-list');
+    const editBtnEl = document.querySelector('.btn.edit');
+    const okBtnEl = document.querySelector('.btn.ok');
+
+    addBtnEl.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        checkListEl.innerHTML = `
+          <li class="laptop-item">
+            <article>
+                <p>${addLaptopFormInputEl[0].value}</p>
+                <p>Memory: ${addLaptopFormInputEl[1].value}</p>
+                <p>Price: ${addLaptopFormInputEl[2].value}</p>
+            </article>
+            <button class="btn edit">edit</button>
+            <button class="btn ok">ok</button>
+          </li>
+          `;
+
+        addLaptopFormInputEl.forEach(el => {
+            el.value = ''
+        });
+        addBtnEl.disabled = true;
+    });
+}
   
