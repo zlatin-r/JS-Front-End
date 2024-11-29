7590f4a89ead30710a4df0247f39613c6902eab1
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-29 07:18:54 +0100

    Solution

--- DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
+++ DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
+    const mainParentEl = document.querySelector('main');
+
+    mainParentEl.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (e.target.textContent === 'onclick') {
+            console.log('clicked');
+        }
+    });
 
 }
