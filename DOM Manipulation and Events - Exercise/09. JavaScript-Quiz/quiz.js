a6ab57003a8538a325f99336039410748c530149
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-29 08:30:56 +0100

    Solution

--- DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
+++ DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
@@ -4,35 +4,40 @@ function solve() {
     const firstQuestion = document.querySelector('main section:nth-of-type(1)')
     const secondQuestion = document.querySelector('main section:nth-of-type(2)');
     const thirdQuestion = document.querySelector('main section:nth-of-type(3)');
-    const allSections = document.querySelectorAll('section');
     const resultsEl = document.querySelector('#results');
 
     let correctAnswers = 0;
 
     firstQuestion.addEventListener('click', (e) => {
         if (e.target.textContent === 'onclick') {
-            correctAnswers += 1;
+            correctAnswers++;
         }
+        firstQuestion.classList.add('hidden');
         secondQuestion.classList.remove('hidden');
     });
 
     secondQuestion.addEventListener('click', (e) => {
         if (e.target.textContent === 'JSON.stringify()') {
-            correctAnswers += 1;
+            correctAnswers++;
         }
+        secondQuestion.classList.add('hidden');
         thirdQuestion.classList.remove('hidden');
     });
 
     thirdQuestion.addEventListener('click', (e) => {
         if (e.target.textContent === 'A programming API for HTML and XML documents') {
-            correctAnswers += 1;
+            correctAnswers++;
         }
+        thirdQuestion.classList.add('hidden');
 
-        resultsEl.textContent = `${correctAnswers}`;
-
+        if (correctAnswers === 3) {
+            resultsEl.textContent = 'You are recognized as top JavaScript fan!';
+        } else if (correctAnswers === 2) {
+            resultsEl.textContent = `You have 2 right answers`;
+        } else {
+            resultsEl.textContent = 'You have 1 right answer';
+        }
     });
-
-    if (correctAnswers === 3) {
-
-    }
 }
+
+
