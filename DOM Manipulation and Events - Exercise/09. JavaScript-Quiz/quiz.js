cd0dd1dfbe9cf210644ce30cd8f5e78b73aec4fb
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-29 07:36:37 +0100

    Solution

--- DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
+++ DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
@@ -2,13 +2,32 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     const mainParentEl = document.querySelector('main');
+    const secondQuestion = document.querySelector('main section:nth-of-type(2)');
+    const thirdQuestion = document.querySelector('main section:nth-of-type(3)');
+    const resultsEl = document.querySelector('#results');
+
+    resultsEl.style.display = 'none';
+
+    let correctAnswers = 0;
+    let totalAnswers = 0;
 
     mainParentEl.addEventListener('click', (e) => {
         e.preventDefault();
 
         if (e.target.textContent === 'onclick') {
-            console.log('clicked');
+            secondQuestion.classList.remove('hidden')
+            correctAnswers++;
         }
+        if (e.target.textContent === 'JSON.stringify()') {
+            thirdQuestion.classList.remove('hidden')
+            correctAnswers++
+        }
+        if (e.target.textContent === 'A programming API for HTML and XML documents') {
+            correctAnswers++
+        }
+
+        resultsEl.textContent = `${correctAnswers}`;
+        resultsEl.style.display = 'block';
     });
 
 }
