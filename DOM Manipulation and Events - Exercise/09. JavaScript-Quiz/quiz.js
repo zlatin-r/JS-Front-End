89d476868d0c28587bd802124e6190c89afb742f
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-29 07:52:05 +0100

    Solution

--- DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
+++ DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/quiz.js
@@ -1,33 +1,38 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    const mainParentEl = document.querySelector('main');
+    const firstQuestion = document.querySelector('main section:nth-of-type(1)')
     const secondQuestion = document.querySelector('main section:nth-of-type(2)');
     const thirdQuestion = document.querySelector('main section:nth-of-type(3)');
+    const allSections = document.querySelectorAll('section');
     const resultsEl = document.querySelector('#results');
 
-    resultsEl.style.display = 'none';
-
     let correctAnswers = 0;
-    let totalAnswers = 0;
 
-    mainParentEl.addEventListener('click', (e) => {
-        e.preventDefault();
-
+    firstQuestion.addEventListener('click', (e) => {
         if (e.target.textContent === 'onclick') {
-            secondQuestion.classList.remove('hidden')
-            correctAnswers++;
+            correctAnswers += 1;
         }
+        secondQuestion.classList.remove('hidden');
+    });
+
+    secondQuestion.addEventListener('click', (e) => {
         if (e.target.textContent === 'JSON.stringify()') {
-            thirdQuestion.classList.remove('hidden')
-            correctAnswers++
+            correctAnswers += 1;
         }
+        thirdQuestion.classList.remove('hidden');
+    });
+
+    thirdQuestion.addEventListener('click', (e) => {
         if (e.target.textContent === 'A programming API for HTML and XML documents') {
-            correctAnswers++
+            correctAnswers += 1;
         }
 
         resultsEl.textContent = `${correctAnswers}`;
-        resultsEl.style.display = 'block';
+
     });
 
+    if (correctAnswers === 3) {
+
+    }
 }
