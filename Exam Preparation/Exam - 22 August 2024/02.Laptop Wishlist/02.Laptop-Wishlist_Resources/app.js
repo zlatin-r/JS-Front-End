82d345271b89f6f739f33d3c12a65e4d36f1be06
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-03 14:46:16 +0100

    Solution

--- Exam Preparation/Exam - 22 August 2024/02.Laptop Wishlist/02.Laptop-Wishlist_Resources/app.js
+++ Exam Preparation/Exam - 22 August 2024/02.Laptop Wishlist/02.Laptop-Wishlist_Resources/app.js
@@ -3,22 +3,23 @@ window.addEventListener("load", solve);
 function solve() {
     const addLaptopFormInputEl = document.querySelectorAll('.laptop-info input');
     const addBtnEl = document.querySelector('.laptop-info button');
+    const clearBtnEl = document.querySelector('.btn.clear');
     const checkListEl = document.querySelector('#check-list');
     const laptopsListEl = document.querySelector('#laptops-list');
 
     addBtnEl.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const model = addLaptopFormInputEl[0].value
-        const memory = addLaptopFormInputEl[1].value
-        const price = addLaptopFormInputEl[2].value
+        const model = addLaptopFormInputEl[0].value.trim();
+        const memory = addLaptopFormInputEl[1].value.trim();
+        const price = addLaptopFormInputEl[2].value.trim();
 
         checkListEl.innerHTML = `
           <li class="laptop-item">
             <article>
                 <p>${model}</p>
-                <p>Memory: ${memory}</p>
-                <p>Price: ${price}</p>
+                <p>Memory: ${memory} TB</p>
+                <p>Price: ${price}$</p>
             </article>
             <button class="btn edit">edit</button>
             <button class="btn ok">ok</button>
@@ -38,9 +39,11 @@ function solve() {
             const article = laptopItem.querySelector('article');
             const model = article.querySelector('p:nth-of-type(1)').textContent;
             const memory = article.querySelector('p:nth-of-type(2)').textContent
-                .replace('Memory: ', '');
+                .replace('Memory: ', '')
+                .replace(' TB', '');
             const price = article.querySelector('p:nth-of-type(3)').textContent
-                .replace('Price: ', '');
+                .replace('Price: ', '')
+                .replace('$', '');
 
             addLaptopFormInputEl[0].value = model;
             addLaptopFormInputEl[1].value = memory;
@@ -48,11 +51,20 @@ function solve() {
 
             addBtnEl.disabled = false;
             laptopItem.remove();
+
         } else if (e.target.classList.contains('ok')) {
             checkListEl.remove();
             laptopsListEl.append(checkListEl);
-        }
 
+            document.querySelectorAll('#laptops-list .btn').forEach((el) => {
+                el.remove();
+            });
+
+            addBtnEl.disabled = false;
+        }
+    })
+    clearBtnEl.addEventListener('click', () => {
+        location.reload();
     });
 }
   
