312f86eb0322f79d1f568b436db519b2690fa606
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-03 16:55:27 +0100

    Initial Commit

--- Exam Preparation/Exam - 18 August 2024/02.Historic Events/02.Historic-Events_Resources/app.js
+++ Exam Preparation/Exam - 18 August 2024/02.Historic Events/02.Historic-Events_Resources/app.js
@@ -25,6 +25,52 @@ function solve() {
         let eventTitleParagraphEl = document.createElement('p');
         let eventDateParagraphEl = document.createElement('p');
         let eventDescriptionParagraphEl = document.createElement('p');
+        let eventDivEl = document.createElement('div');
+        let eventBtnEditEl = document.createElement('button');
+        let eventBtnNextEl = document.createElement('button');
+
+        eventDivEl.classList.add('buttons');
+        eventBtnEditEl.classList.add('edit-btn');
+        eventBtnEditEl.textContent = 'Edit';
+        eventBtnEditEl.addEventListener('click', edit);
+        eventBtnNextEl.classList.add('next-btn');
+        eventBtnNextEl.textContent = 'Next';
+
+        eventTitleParagraphEl.textContent = eventNameEl.value;
+        eventDateParagraphEl.textContent = dateTimeEl.value;
+        eventDescriptionParagraphEl.textContent = descriptionEl.value;
+
+        let title = eventNameEl.value;
+        let time = dateTimeEl.value;
+        let description = descriptionEl.value;
+
+        eventArticleEl.appendChild(eventTitleParagraphEl);
+        eventArticleEl.appendChild(eventDateParagraphEl);
+        eventArticleEl.appendChild(eventDescriptionParagraphEl);
+
+        eventDivEl.appendChild(eventBtnEditEl);
+        eventDivEl.appendChild(eventBtnNextEl);
+
+        eventLiEl.appendChild(eventArticleEl);
+        eventLiEl.appendChild(eventDivEl);
+
+        previewListEl.appendChild(eventLiEl);
+
+        addBtnEl.disabled = true;
+
+        eventNameEl.value = ""
+        dateTimeEl.value = ""
+        descriptionEl.value = ""
+
+        function edit() {
+            eventNameEl.value = title;
+            dateTimeEl.value = time;
+            descriptionEl.value = description;
+
+            previewListEl.removeChild(eventLiEl);
+
+            addBtnEl.disabled = false;
+        }
 
     }
 }
