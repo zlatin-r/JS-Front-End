0ab5ffa74bc1a62626f144be92836fe12bc47f1d
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-03 16:25:13 +0100

    Initial Commit

--- Exam Preparation/Exam - 18 August 2024/02.Historic Events/02.Historic-Events_Resources/app.js
+++ Exam Preparation/Exam - 18 August 2024/02.Historic Events/02.Historic-Events_Resources/app.js
@@ -1,5 +1,23 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO....
+    let eventNameEl = document.querySelector('#name');
+    let dateTimeEl = document.querySelector('#time');
+    let descriptionEl = document.querySelector('#description');
+    let addBtnEl = document.querySelector('#add-btn');
+
+    addBtnEl.addEventListener('click', (event) => {
+        event.preventDefault();
+        publish()
+    });
+
+    function publish() {
+        if (eventNameEl.value === "" ||
+            dateTimeEl.value === "" ||
+            descriptionEl.value === "") {
+            return;
+        }
+
+        console.log(eventNameEl.value, dateTimeEl.value, descriptionEl.value);
+    }
 }
