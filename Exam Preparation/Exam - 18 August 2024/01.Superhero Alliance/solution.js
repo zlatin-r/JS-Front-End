0bd544a5e4bf2ebb3bf307cf3c931a135e38c392
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-03 15:37:10 +0100

    Solution

--- Exam Preparation/Exam - 18 August 2024/01.Superhero Alliance/solution.js
+++ Exam Preparation/Exam - 18 August 2024/01.Superhero Alliance/solution.js
@@ -1,13 +1,29 @@
 function solve(arr) {
     const n = Number(arr.shift());
-    const heroes = [];
+    const heroes = {};
 
     for (let i = 0; i < n; i++) {
-        const [heroName, superPowers, energy] = arr[i].split('-');
-        const newHero = {name: heroName, powers: superPowers.split(','), energy: energy};
-        heroes.push(newHero);
+        const [heroName, superPowers, energy] = arr.shift().split('-');
+        heroes[heroName] = {};
+        heroes[heroName] = {powers: superPowers.split(','), energy: Number(energy)};
     }
-
+    for (let row of arr) {
+        if (!row.includes('Evil Defeated!')) {
+            if (row.includes('Use Power')) {
+                let [action, name, power, energy] = row.split(' * ');
+                    if (heroes[name].powers.includes(power) && heroes[name].energy >= Number(energy)) {
+                        heroes[name].energy -= Number(energy);
+                        console.log(`${name} has used ${power} and now has ${heroes[name].energy} energy!`);
+                    } else {
+                        console.log(`${name} is unable to use ${power} or lacks energy!`);
+                    }
+            } else if (row.includes('Train')) {
+                let [action, name, energy] = row.split(' * ');
+                const currEnergy = heroes[name].energy;
+                heroes[name].energy = Math.min(100, currEnergy + energy);
+            }
+        }
+    }
 }
 
 solve([
