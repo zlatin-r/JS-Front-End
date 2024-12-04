093fe8397246bd807d5f2090c2a7f468d316f9c0
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-04 14:46:09 +0100

    Initial Commit

--- HTTP and REST/03. Github-Repos-by-Username/scripts/app.js
+++ HTTP and REST/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,24 @@
 function loadRepos() {
-	console.log("TODO...");
+    const userName = document.getElementById("username").value;
+    const listEl = document.querySelector('#repos');
+    const url = `https://api.github.com/users/${userName}/repos`;
+
+    listEl.textContent = '';
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+
+            data.forEach((item) => {
+                let newLiEl = document.createElement("li");
+                let newAEl = document.createElement("a");
+
+                newAEl.textContent = item.full_name;
+                newAEl.setAttribute("href", item.html_url);
+
+                newLiEl.appendChild(newAEl);
+                listEl.appendChild(newLiEl);
+            });
+        })
+        .catch((error) => console.log(error));
 }
