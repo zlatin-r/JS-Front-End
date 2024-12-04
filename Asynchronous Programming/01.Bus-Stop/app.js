e8be2e5b6f82ffa01d47edddf0bb55d1b1988593
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-04 16:08:31 +0100

    Solution

--- Asynchronous Programming/01.Bus-Stop/app.js
+++ Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,4 +1,23 @@
 function getInfo() {
     const stopId = document.getElementById('stopId').value;
+    const resultDivEl = document.querySelector('#stopName');
+    const resultUlEl = document.querySelector('#buses');
 
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    const responsePromise = fetch(url);
+
+    responsePromise
+        .then(response => response.json())
+        .then(data => {
+            resultDivEl.textContent = data.name;
+            Object.entries(data.buses).forEach(([busId, time]) => {
+                const newLiEl = document.createElement('li');
+                newLiEl.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                resultUlEl.appendChild(newLiEl);
+            })
+        }).catch(err => {
+            resultDivEl.textContent = 'Error';
+            resultUlEl.innerHTML = '';
+        });
 }
