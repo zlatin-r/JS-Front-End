6fcaa0c38f3d10dae5cebd4e1f5134b6674817dd
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-04 15:15:47 +0100

    Initial Commit

--- HTTP and REST/04. Github-Commits/app.js
+++ HTTP and REST/04. Github-Commits/app.js
@@ -1,4 +1,24 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const userName = document.querySelector('#username').value;
+    const repoName = document.querySelector('#repo').value;
+
+    const commitsListEl = document.querySelector('#commits');
+
+    const url = `https://api.github.com/repos/${userName}/${repoName}/commits`;
+
+    const responsePromise = fetch(url)
+    responsePromise
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach(commit => {
+                const newLiEl = document.createElement('li');
+                newLiEl.textContent = `${commit.commit.author.name}:${commit.commit.message}`;
+
+                commitsListEl.appendChild(newLiEl);
+            });
+        }).catch((error) => {
+            const li = document.createElement("li");
+            li.textContent = `Error: ${error.message}`;
+            commitsListEl.appendChild(li);
+    });
 }
