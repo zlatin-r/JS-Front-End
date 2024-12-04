3dc221d04b7fe1d043c9c21d54c27bc606cde575
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-04 17:19:43 +0100

    Solution

--- Asynchronous Programming/03.Forecaster/app.js
+++ Asynchronous Programming/03.Forecaster/app.js
@@ -1,5 +1,22 @@
 function attachEvents() {
-    // TODO:
+    const getWeatherBtnEl = document.getElementById('submit');
+    const forecastEl = document.getElementById('forecast');
+    const url = "http://localhost:3030/jsonstore/forecaster/locations"
+
+    getWeatherBtnEl.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const location = document.getElementById('location').value;
+        const responsePromise = fetch(url);
+
+        responsePromise
+            .then(response => response.json())
+            .then(data => {
+                let currLocation = data.filter(item => item.name === location);
+            })
+
+        forecastEl.style.display = 'block';
+    });
 }
 
 attachEvents();
