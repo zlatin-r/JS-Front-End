21e410a785d5d405b7a77932a965b5a431c05619
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-05 19:31:11 +0100

    Solution

--- HTTP and AJAX - Exercise/02.Messenger/app.js
+++ HTTP and AJAX - Exercise/02.Messenger/app.js
@@ -1,5 +1,14 @@
 function attachEvents() {
-    // TODO:
+    const textAreaEl = getElementByTag("#textarea");
+    const authorNameEl = document.querySelector('input[name="author"]');
+    const messageContentEl = document.querySelector('input[name="content"]');
+    const sendBtnEl = getElementByTag('#submit')
+    const baseUrl ='http://localhost:3030/jsonstore/messenger'
+
+}
+
+function getElementByTag(tag) {
+    return document.querySelector(tag);
 }
 
 attachEvents();
