ace34c1b12aa0cb3906245a023886faf13794b81
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-05 21:13:38 +0100

    Solution

--- HTTP and AJAX - Exercise/03.Phonebook/app.js
+++ HTTP and AJAX - Exercise/03.Phonebook/app.js
@@ -10,7 +10,17 @@ function attachEvents() {
     loadBtnEl.addEventListener('click', loadAllContacts);
     createBtnEl.addEventListener('click', createContact);
 
+    phoneBookEl.addEventListener('click', async (e) => {
+        if (e.target.classList.contains('btnDelete')) {
+            const response = await fetch(`${baseUrl}/${e.target.id}`, {method: 'DELETE'});
+            e.target.parentElement.remove();
+        }
+    })
+
     async function loadAllContacts() {
+
+        phoneBookEl.innerHTML = '';
+
         const response = await fetch(baseUrl);
         const data = await response.json();
 
@@ -20,6 +30,8 @@ function attachEvents() {
 
             const newDelBtnEl = createElement('button');
             newDelBtnEl.textContent = 'Delete';
+            newDelBtnEl.id = contact._id;
+            newDelBtnEl.classList.add('btnDelete');
 
             newLiEl.appendChild(textNode);
             newLiEl.appendChild(newDelBtnEl);
@@ -29,7 +41,22 @@ function attachEvents() {
     }
 
     async function createContact() {
+        const personName = nameInputEl.value;
+        const phoneNumber = phoneInputEl.value;
 
+        const contact = {
+            person: personName,
+            phone: phoneNumber,
+        }
+
+        const response = await fetch(baseUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(contact),
+        });
+
+        nameInputEl.value = '';
+        phoneInputEl.value = '';
     }
 }
 
