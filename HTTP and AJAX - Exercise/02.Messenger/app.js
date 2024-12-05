45a1e20d26f9c4eaeed32630e93c130706d94ec5
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-05 19:41:48 +0100

    Solution

--- HTTP and AJAX - Exercise/02.Messenger/app.js
+++ HTTP and AJAX - Exercise/02.Messenger/app.js
@@ -1,9 +1,38 @@
 function attachEvents() {
     const textAreaEl = getElementByTag("#textarea");
-    const authorNameEl = document.querySelector('input[name="author"]');
-    const messageContentEl = document.querySelector('input[name="content"]');
+    const authorNameEl = getElementByTag('input[name="author"]');
+    const messageContentEl = getElementByTag('input[name="content"]');
     const sendBtnEl = getElementByTag('#submit')
-    const baseUrl ='http://localhost:3030/jsonstore/messenger'
+    const refreshBtnEl = getElementByTag('#refresh')
+
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger'
+
+    sendBtnEl.addEventListener('click', postMessage);
+    refreshBtnEl.addEventListener('click', refreshPage);
+
+    async function postMessage() {
+        const authorName = authorNameEl.value;
+        const messageContent = messageContentEl.value;
+
+        const message = {
+            author: authorName,
+            message: messageContent
+        };
+
+        try {
+            const response = await fetch(baseUrl, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(message),
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function refreshPage() {
+
+    }
 
 }
 
