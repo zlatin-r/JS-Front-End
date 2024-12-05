3c43e5f43bec20646413b901a748eab9a0071f12
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-05 14:12:33 +0100

    Solution

--- Asynchronous Programming/03.Forecaster/app.js
+++ Asynchronous Programming/03.Forecaster/app.js
@@ -1,19 +1,65 @@
 function attachEvents() {
-    const getWeatherBtnEl = document.getElementById('submit');
-    const forecastEl = document.getElementById('forecast');
-    const currentEl = document.getElementById('current');
-    const upcomingEl = document.getElementById('upcoming');
+    const locationInput = document.getElementById('location');
+    const submitButton = document.getElementById('submit');
+    const forecastDiv = document.getElementById('forecast');
+    const currentDiv = document.getElementById('current');
+    const upcomingDiv = document.getElementById('upcoming');
 
-    const baseUrl = "http://localhost:3030/jsonstore/forecaster";
+    const symbols = {
+        Sunny: '☀',
+        'Partly sunny': '⛅',
+        Overcast: '☁',
+        Rain: '☂',
+        Degrees: '°'
+    };
+    submitButton.addEventListener('click', async () => {
+        const locationName = locationInput.value;
 
-    getWeatherBtnEl.addEventListener('click', (e) => {
-        e.preventDefault();
+        try {
+            const locationsResponse = await fetch('http://localhost:3030/jsonstore/forecaster/locations');
+            const locations = await locationsResponse.json();
 
-        const requestedLocation = document.querySelector('#location').value;
+            const location = locations.find(loc => loc.name === locationName);
+            const locationCode = location.code;
 
-        console.log(requestedLocation);
-    })
+            const todayResponse = await fetch(`http://localhost:3030/jsonstore/forecaster/today/${locationCode}`);
+            const todayData = await todayResponse.json();
 
+            const upcomingResponse = await fetch(`http://localhost:3030/jsonstore/forecaster/upcoming/${locationCode}`);
+            const upcomingData = await upcomingResponse.json();
+
+            displayForecast(todayData, upcomingData);
+        } catch (error) {
+            forecastDiv.style.display = 'block';
+            forecastDiv.innerHTML = '<div class="label">Error</div>';
+        }
+    });
+
+    function displayForecast(todayData, upcomingData) {
+        forecastDiv.style.display = 'block';
+
+        const todayElement = document.createElement('div');
+        todayElement.className = 'forecasts';
+
+        const conditionSymbolSpan = document.createElement('span');
+        conditionSymbolSpan.className = 'condition symbol';
+        conditionSymbolSpan.textContent = `${symbols[todayData.forecast.condition]}`
+
+        const conditionSpan = document.createElement('span');
+        conditionSpan.className = 'condition';
+
+        const locationSpan = document.createElement('span');
+        locationSpan.className = 'forecast-data';
+        locationSpan.textContent = `${todayData.name}`;
+
+        const degreeSpan = document.createElement('span');
+        degreeSpan.className = 'forecast-data';
+        degreeSpan.textContent = `${todayData.forecast.low}${symbols.Degrees}/${todayData.forecast.high}${symbols.Degrees}`
+
+        const forecastSpan = document.createElement('span');
+        forecastSpan.className = 'forecast-data';
+        forecastSpan.textContent = `${todayData.name}`;
+    }
 }
 
 attachEvents();
