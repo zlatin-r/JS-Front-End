f3a063301c43ec62e3a60a32b06a46972bfbdb45
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-05 20:15:00 +0100

    Solution

--- HTTP and AJAX - Exercise/03.Phonebook/app.js
+++ HTTP and AJAX - Exercise/03.Phonebook/app.js
@@ -3,11 +3,18 @@ function attachEvents() {
     const createBtnEl = takeElementByTag('#btnCreate');
     const nameInputEl = takeElementByTag('#person');
     const phoneInputEl = takeElementByTag('#phone');
+    const phoneBookEl = takeElementByTag('#phonebook');
+
+    const baseUrl = 'http://localhost:3030/jsonstore/phonebook'
 
     loadBtnEl.addEventListener('click', loadAllContacts);
     createBtnEl.addEventListener('click', createContact);
 
     async function loadAllContacts() {
+        const response = await fetch(baseUrl);
+        const data = await response.json();
+
+
 
     }
 
@@ -18,6 +25,10 @@ function attachEvents() {
 
 function takeElementByTag(tag) {
     return document.querySelector(tag);
+}
+
+function createElement(tag) {
+    return document.createElement(tag);
 }
 
 attachEvents();
