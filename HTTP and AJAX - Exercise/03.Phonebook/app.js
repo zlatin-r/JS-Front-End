e799c68ed9178b56e013ae5ee4afa5f909709200
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-05 20:09:09 +0100

    Solution

--- HTTP and AJAX - Exercise/03.Phonebook/app.js
+++ HTTP and AJAX - Exercise/03.Phonebook/app.js
@@ -1,5 +1,23 @@
 function attachEvents() {
-    // TODO:
+    const loadBtnEl = takeElementByTag('#btnLoad');
+    const createBtnEl = takeElementByTag('#btnCreate');
+    const nameInputEl = takeElementByTag('#person');
+    const phoneInputEl = takeElementByTag('#phone');
+
+    loadBtnEl.addEventListener('click', loadAllContacts);
+    createBtnEl.addEventListener('click', createContact);
+
+    async function loadAllContacts() {
+
+    }
+
+    async function createContact() {
+
+    }
+}
+
+function takeElementByTag(tag) {
+    return document.querySelector(tag);
 }
 
 attachEvents();
