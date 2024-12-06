f9d725eb946c44bb2e1ea51a159eba7ed0cf6816
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-06 16:43:13 +0100

    Solution

--- HTTP and AJAX - Exercise/06. Accordion/accordion.js
+++ HTTP and AJAX - Exercise/06. Accordion/accordion.js
@@ -1,3 +1,32 @@
 function solution() {
-    //TODO .....
+    const mainEl = document.querySelector('#main');
+
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list'
+
+    mainEl.innerHTML = ``
+
+    loadArticles()
+
+    async function loadArticles() {
+        const response = await fetch(url);
+        const articles = await response.json();
+
+        Object.values(articles).forEach(article => {
+            const newDivEl = document.createElement('div');
+            newDivEl.className = 'accordion';
+
+            newDivEl.innerHTML = `
+                <div class="head">
+                <span>${article.title}</span>
+                <button class="button" id="${article._id}">More</button>
+                </div>
+                <div class="extra">
+                    <p>Scalable Vector Graphics .....</p>
+                </div>
+            `
+            mainEl.appendChild(newDivEl);
+        })
+    }
 }
+
+solution();
