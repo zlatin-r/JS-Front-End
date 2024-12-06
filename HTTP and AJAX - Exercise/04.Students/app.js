01b770a94e65a2df3a7c2ddff780fb4c2f65337b
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-06 10:37:59 +0100

    Solution

--- HTTP and AJAX - Exercise/04.Students/app.js
+++ HTTP and AJAX - Exercise/04.Students/app.js
@@ -1,8 +1,5 @@
 function attachEvents() {
-    const firstNameInputEl = takeElementByTag('input[name="firstName"]');
-    const lastNameInputEl = takeElementByTag('input[name="lastName"]');
-    const facultyNumberInputEl = takeElementByTag('input[name="facultyNumber"]');
-    const gradeInputEl = takeElementByTag('input[name="grade"]');
+    const tableEl = takeElementByTag('#results tbody');
     const submitBtnEl = takeElementByTag('#submit');
 
     const baseUrl = 'http://localhost:3030/jsonstore/collections/students';
@@ -11,17 +8,51 @@ function attachEvents() {
 
     async function createStudent() {
 
-        const studentInfo = {
-            firstName: firstNameInputEl.value,
-            lastName: lastNameInputEl.value,
-            facultyNumber: facultyNumberInputEl.value,
-            grade: gradeInputEl.value,
+        const firstName = takeElementByTag('input[name="firstName"]').value.trim();
+        const lastName = takeElementByTag('input[name="lastName"]').value.trim();
+        const facultyNumber = takeElementByTag('input[name="facultyNumber"]').value.trim();
+        const grade = takeElementByTag('input[name="grade"]').value.trim();
+
+        if (!firstName || !lastName || !facultyNumber || grade) {
+            return
         }
+
+        const studentInfo = {
+            firstName,
+            lastName,
+            facultyNumber,
+            grade: Number(grade),
+        };
 
         await fetch(baseUrl, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(studentInfo),
+        });
+
+        const response = await fetch(baseUrl);
+        const data = await response.json();
+
+        console.log(data);
+
+        Object.entries(data).forEach(([key, value]) => {
+            const newTrEl = createElement('tr');
+
+            const firstNameTdEl = createElement('td');
+            firstNameTdEl.textContent = value.firstName;
+            const lastNameTdEl = createElement('td');
+            lastNameTdEl.textContent = value.lastName;
+            const facultyNumTdEl = createElement('td');
+            facultyNumTdEl.textContent = value.facultyNumber;
+            const gradeTdEl = createElement('td');
+            gradeTdEl.textContent = value.grade;
+
+            newTrEl.appendChild(firstNameTdEl);
+            newTrEl.appendChild(lastNameTdEl);
+            newTrEl.appendChild(facultyNumTdEl);
+            newTrEl.appendChild(gradeTdEl);
+
+            tableEl.appendChild(newTrEl);
         });
     }
 }
