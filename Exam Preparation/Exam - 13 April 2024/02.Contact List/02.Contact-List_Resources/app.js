eec91953522114b3fb3a3d2b1bbea6425899fdee
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-08 13:51:25 +0100

    Solution

--- Exam Preparation/Exam - 13 April 2024/02.Contact List/02.Contact-List_Resources/app.js
+++ Exam Preparation/Exam - 13 April 2024/02.Contact List/02.Contact-List_Resources/app.js
@@ -1,6 +1,52 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //...TODO
-  }
-  
+    const nameInputEl = document.querySelector('#name');
+    const phoneInputEl = document.querySelector('#phone');
+    const categoryInputEl = document.querySelector('#category');
+
+    const addBtnEl = document.querySelector('#add-btn');
+
+    const checkListEl = document.querySelector('#check-list');
+
+    addBtnEl.addEventListener('click', addBtnHandler)
+
+    function clearInput() {
+        nameInputEl.value = '';
+        phoneInputEl.value = '';
+        categoryInputEl.value = '';
+    }
+
+    function addBtnHandler() {
+
+        const name = nameInputEl.value.trim();
+        const phone = phoneInputEl.value.trim();
+        const category = categoryInputEl.value;
+
+        if (!name || !phone || !category) {
+            return;
+        }
+
+        const newContact = document.createElement('li');
+
+        newContact.innerHTML = `
+        <li>
+            <article>
+                <p>name: ${name}</p>
+                <p>phone: ${phone}</p>
+                <p>category: ${category}</p>
+            </article>
+            <div class="buttons">
+                <button class="edit-btn"></button>
+                <button class="save-btn"></button>
+            </div>
+        </li>
+        `
+
+        checkListEl.append(newContact);
+
+        clearInput();
+    }
+
+}
+
