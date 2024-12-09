ffb9b9402c2e53f913f1eed0626fabdc3e645cf2
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-09 15:27:40 +0100

    Initial Commit

--- Exam Preparation/Exam - 22 December 2023/03.Christmas Gifts/03.Christmas-Gifts_Resources/app.js
+++ Exam Preparation/Exam - 22 December 2023/03.Christmas Gifts/03.Christmas-Gifts_Resources/app.js
@@ -1,1 +1,53 @@
-//TODO...
+const baseUrl = 'http://localhost:3030/jsonstore/gifts'
+const endpoints = {
+    update: (id) => `${baseUrl}/${id}`,
+    delete: (id) => `${baseUrl}/${id}`,
+}
+
+const giftListEl = document.querySelector('#gift-list');
+
+const loadPresentsBtnEl = document.querySelector('#load-presents');
+
+function attachEvents() {
+    loadPresentsBtnEl.addEventListener('click', loadPresents);
+}
+
+async function loadPresents() {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+
+    Object.values(data).forEach((item) => {
+        const newGiftSock = document.createElement('div');
+        newGiftSock.className = 'gift-sock';
+
+        const contentDiv = document.createElement('div');
+        contentDiv.className = 'content';
+
+        const giftParagraph = document.createElement('p');
+        giftParagraph.textContent = item.gift;
+
+        const forParagraph = document.createElement('p');
+        forParagraph.textContent = item.for;
+
+        const priceParagraph = document.createElement('p');
+        priceParagraph.textContent = item.price;
+
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.className = 'buttons-container';
+
+        const changeButton = document.createElement('button');
+        changeButton.className = 'change-btn';
+        changeButton.textContent = 'Change';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-btn';
+        deleteButton.textContent = 'Delete';
+
+
+
+
+    })
+}
+
+
+attachEvents();
