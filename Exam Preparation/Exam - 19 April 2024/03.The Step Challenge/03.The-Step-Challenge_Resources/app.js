c99183fdcbfd24fccbff004db929a8dde078ec65
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-09 12:47:14 +0100

    Initial Commit

--- Exam Preparation/Exam - 19 April 2024/03.The Step Challenge/03.The-Step-Challenge_Resources/app.js
+++ Exam Preparation/Exam - 19 April 2024/03.The Step Challenge/03.The-Step-Challenge_Resources/app.js
@@ -1,1 +1,22 @@
-//TODO...
+const baseUrl = 'http://localhost:3030/jsonstore/records'
+const endpoints = {
+    update: (id) => `${baseUrl}/${id}`,
+    delete: (id) => `${baseUrl}/${id}`,
+}
+
+const loadRecordsBtnEl = document.querySelector('#load-records');
+
+function attachEvents() {
+    loadRecordsBtnEl.addEventListener('click', loadRecords);
+}
+
+async function loadRecords() {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+
+    Object.values(data).forEach((record) => {
+
+    })
+}
+
+attachEvents();
