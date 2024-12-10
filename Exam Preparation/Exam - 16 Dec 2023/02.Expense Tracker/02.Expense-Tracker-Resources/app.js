dfc927c11f48f8a4749012d252af53486d6cb95e
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-10 11:06:07 +0100

    Initial Commit

--- Exam Preparation/Exam - 16 Dec 2023/02.Expense Tracker/02.Expense-Tracker-Resources/app.js
+++ Exam Preparation/Exam - 16 Dec 2023/02.Expense Tracker/02.Expense-Tracker-Resources/app.js
@@ -1,3 +1,60 @@
 window.addEventListener("load", solve);
 
-//TODO
+const expenseInputEl = document.querySelector("#expense");
+const amountInputEl = document.querySelector("#amount");
+const dateInputEl = document.querySelector("#date");
+
+const previewListEl = document.querySelector("#preview-list");
+
+const addBtnEl = document.querySelector("#add-btn");
+
+function solve() {
+
+    addBtnEl.addEventListener("click", addBtnClickHandler);
+}
+
+function addBtnClickHandler() {
+    if (expenseInputEl.value === "" || amountInputEl.value === "" || dateInputEl.value === "") {
+        return;
+    }
+
+    const newExpenseItem = document.createElement("li");
+    newExpenseItem.className = "expense-item";
+
+    const articleEl = document.createElement("article");
+
+    const typeEl = document.createElement("p");
+    typeEl.textContent = `Type: ${expenseInputEl.value}`;
+
+    const amountEl = document.createElement("p");
+    amountEl.textContent = `Amount: ${amountInputEl.value}$`;
+
+    const dateEl = document.createElement("p");
+    dateEl.textContent = `Date: ${dateInputEl.value}`;
+
+    const buttonWrapperEl = document.createElement("div");
+    buttonWrapperEl.className = "buttons";
+
+    const editBtnEl = document.createElement("button");
+    editBtnEl.className = "btn edit";
+    editBtnEl.textContent = `edit`;
+
+    const okBtnEl = document.createElement("button");
+    okBtnEl.className = "btn ok";
+    okBtnEl.textContent = `ok`;
+
+    buttonWrapperEl.append(editBtnEl);
+    buttonWrapperEl.append(okBtnEl);
+
+    articleEl.append(typeEl);
+    articleEl.append(amountEl);
+    articleEl.append(dateEl);
+
+    newExpenseItem.append(articleEl);
+    newExpenseItem.append(buttonWrapperEl);
+
+    previewListEl.append(newExpenseItem);
+
+
+
+}
