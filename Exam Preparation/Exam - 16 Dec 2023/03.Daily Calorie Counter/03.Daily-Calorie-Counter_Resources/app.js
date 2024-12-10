79c5f4f20fe0526fa90b65440b46b6ede4af4d14
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-10 14:03:29 +0100

    Initial Commit

--- Exam Preparation/Exam - 16 Dec 2023/03.Daily Calorie Counter/03.Daily-Calorie-Counter_Resources/app.js
+++ Exam Preparation/Exam - 16 Dec 2023/03.Daily Calorie Counter/03.Daily-Calorie-Counter_Resources/app.js
@@ -1,1 +1,35 @@
-//TODO
+const baseUrl = 'http://localhost:3030/jsonstore/tasks';
+
+const endpoints = {
+    update: (id) => `${baseUrl}/${id}`,
+    delete: (id) => `${baseUrl}/${id}`,
+}
+
+const mealListEl = document.querySelector('#list');
+
+const loadMealsBtnEl = document.querySelector('#load-meals');
+
+function attachEvents() {
+    loadMealsBtnEl.addEventListener('click', loadAllMeals)
+}
+
+async function loadAllMeals() {
+    clearMealList();
+
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+
+    Object.values(data).forEach((meal) => {
+        const newMealEl = document.createElement('div');
+        newMealEl.className = "meal";
+
+        const foodType = document.querySelector('#food');
+        foodType.innerText = meal.name;
+    });
+}
+
+//---------------------------- Helpers ---------------------------------------------------
+function clearMealList() {
+    mealListEl.innerHTML = '';
+}
+attachEvents();
