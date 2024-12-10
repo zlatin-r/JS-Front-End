79daaae300295fbff4991629cbbd63d434959c4d
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-10 17:21:12 +0100

    Solution

--- Exam Preparation/Exam - 17 August 2023/03.Weather Tracker/03.Weather Tracker – Resources/app.js
+++ Exam Preparation/Exam - 17 August 2023/03.Weather Tracker/03.Weather Tracker – Resources/app.js
@@ -14,8 +14,12 @@ const loadHistoryBtnEl = document.getElementById('load-history');
 const addButtonEl = document.getElementById('add-weather');
 const editButtonEl = document.getElementById('edit-weather');
 
+let recordId = null;
+
 function attachEvents() {
-    loadHistoryBtnEl.addEventListener('click', loadAllRecords)
+    loadHistoryBtnEl.addEventListener('click', loadAllRecords);
+    addButtonEl.addEventListener('click', addRecord);
+    editButtonEl.addEventListener('click', editRecord)
 }
 
 async function loadAllRecords() {
@@ -58,11 +62,111 @@ async function loadAllRecords() {
         newContainer.appendChild(buttonsContainerEl);
 
         recordsListEl.appendChild(newContainer);
+    });
+    attachEventListeners();
+}
 
+function addRecord() {
+
+    const data = {
+        location: locationInputEl.value,
+        date: dateInputEl.value,
+        temperature: temperatureInputEl.value,
+    }
+
+    fetch(baseUrl, {
+        method: 'POST',
+        body: JSON.stringify(data),
+    }).then(() => {
+        loadAllRecords();
+        clearAllInputs();
     });
 }
 
+function attachEventListeners() {
+    const changeButtonsEl = document.querySelectorAll('.change-btn');
+    const deleteButtonsEl = document.querySelectorAll('.delete-btn');
+
+    changeButtonsEl.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const currRecord = e.target.closest('.container');
+            const location = currRecord.querySelector('h2').textContent;
+            const date = currRecord.querySelector('h3:first-of-type').textContent;
+            const temperature = currRecord.querySelector('h3:last-of-type').textContent;
+            changeRecord(location, date, temperature);
+            enableEditBtn();
+        });
+    });
+    deleteButtonsEl.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const currRecord = e.target.closest('.container');
+            const location = currRecord.querySelector('h2').textContent;
+            deleteRecord(location);
+        })
+    })
+}
+
+async function changeRecord(l, d, t) {
+    recordId = await getIdByLocation(l);
+
+    locationInputEl.value = l;
+    dateInputEl.value = d;
+    temperatureInputEl.value = t;
+}
+
+function editRecord() {
+    const data = {
+        location: locationInputEl.value,
+        date: dateInputEl.value,
+        temperature: temperatureInputEl.value,
+        _id: recordId,
+    }
+
+    fetch(endPoints.update(data._id), {
+        method: 'PUT',
+        body: JSON.stringify(data),
+    }).then(() => {
+        loadAllRecords();
+        clearAllInputs();
+        enableAddBtn();
+        recordId = null;
+    });
+}
+
+function deleteRecord(l) {
+    getIdByLocation(l)
+        .then(id =>
+            fetch(endPoints.delete(id), {
+                method: 'DELETE',
+            }))
+        .then(() => {
+            loadAllRecords();
+        });
+}
+
 // ------------------ Helpers ---------------------------------------
+
+function getIdByLocation(l) {
+    return fetch(baseUrl)
+        .then(res => res.json())
+        .then(res => Object.entries(res).find(r => r[1].location === l)[1]._id);
+}
+function enableAddBtn() {
+    editButtonEl.disabled = true;
+    addButtonEl.disabled = false;
+}
+
+
+function enableEditBtn(){
+    addButtonEl.disabled = true;
+    editButtonEl.disabled = false;
+}
+
+function clearAllInputs() {
+    locationInputEl.value = '';
+    temperatureInputEl.value = '';
+    dateInputEl.value = '';
+}
 function clearAllRecords() {
     recordsListEl.innerHTML = '';
 }
