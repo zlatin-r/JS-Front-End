29baf633c74cf207480ea255721c452d29552247
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-10 10:47:49 +0100

    Solution

--- Exam Preparation/Exam - 16 Dec 2023/01.Cafeteria/solution.js
+++ Exam Preparation/Exam - 16 Dec 2023/01.Cafeteria/solution.js
@@ -4,17 +4,50 @@ function solve(arr) {
 
     for (let i = 0; i < n; i++) {
         const [name, shift, coffeeTypes] = arr.shift().split(' ');
-        team[name] = {shift: shift, coffeeType: coffeeTypes.split(', ')};
+        team[name] = {shift: shift, coffeeType: coffeeTypes.split(',')};
     }
 
-    const data = arr.shift().split(' / ');
-    const action = data[0];
+    let data = arr.shift();
 
-    while (action !== 'Closed') {
+    while (data !== 'Closed') {
+        let parts = data.split(' / ');
+        const action = parts[0];
+
         if (action === 'Prepare') {
+            const baristaName = parts[1];
+            const baristaShift = parts[2];
+            const coffee = parts[3];
 
+            if (team[baristaName].shift === baristaShift && team[baristaName].coffeeType.includes(coffee)) {
+                console.log(`${baristaName} has prepared a ${coffee} for you!`);
+            } else {
+                console.log(`${baristaName} is not available to prepare a ${coffee}.`);
+            }
+        } else if (action === 'Change Shift') {
+            const baristaName = parts[1];
+            const newShift = parts[2];
+
+            team[baristaName].shift = newShift;
+
+            console.log(`${baristaName} has updated his shift to: ${newShift}`);
+
+        } else if (action === 'Learn') {
+            const baristaName = parts[1];
+            const newCoffee = parts[2];
+
+            if (team[baristaName].coffeeType.includes(newCoffee)) {
+                console.log(`${baristaName} knows how to make ${newCoffee}.`);
+            } else {
+                team[baristaName].coffeeType.push(newCoffee);
+                console.log(`${baristaName} has learned a new coffee type: ${newCoffee}.`);
+            }
         }
+        data = arr.shift();
     }
+
+    Object.keys(team).forEach(key => {
+        console.log(`Barista: ${key}, Shift: ${team[key].shift}, Drinks: ${team[key].coffeeType.join(', ')}`);
+    });
 }
 
 solve([
