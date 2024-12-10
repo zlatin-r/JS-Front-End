8996dc263be270d06e3737d96aeff8cb231ecb26
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-10 15:14:26 +0100

    Solution

--- Exam Preparation/Exam - 16 Dec 2023/03.Daily Calorie Counter/03.Daily-Calorie-Counter_Resources/app.js
+++ Exam Preparation/Exam - 16 Dec 2023/03.Daily Calorie Counter/03.Daily-Calorie-Counter_Resources/app.js
@@ -8,9 +8,19 @@ const endpoints = {
 const mealListEl = document.querySelector('#list');
 
 const loadMealsBtnEl = document.querySelector('#load-meals');
+const addMealBtnEl = document.querySelector('#add-meal');
+const editMealBtnEl = document.querySelector('#edit-meal');
+
+const foodInputEl = document.querySelector('#food');
+const timeInputEl = document.querySelector('#time');
+const caloriesInputEl = document.querySelector('#calories');
+
+let mealId = null;
 
 function attachEvents() {
-    loadMealsBtnEl.addEventListener('click', loadAllMeals)
+    loadMealsBtnEl.addEventListener('click', loadAllMeals);
+    addMealBtnEl.addEventListener('click', addMeal);
+    editMealBtnEl.addEventListener('click', editMeal);
 }
 
 async function loadAllMeals() {
@@ -54,10 +64,115 @@ async function loadAllMeals() {
 
         mealListEl.appendChild(newMealEl);
     });
+
+    addEventListeners();
 }
 
+function addEventListeners() {
+    const changeButtonsEl = document.querySelectorAll('.change-meal');
+    const deleteButtonsEl = document.querySelectorAll('.delete-meal');
+
+    changeButtonsEl.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const currMealEl = e.target.closest('.meal');
+            const food = currMealEl.querySelector('h2').textContent;
+            const time = currMealEl.querySelector('h3:first-of-type').textContent;
+            const calories = currMealEl.querySelector('h3:last-of-type').textContent;
+            populateInputFields(food, time, calories);
+            enableEditBtn();
+        });
+    });
+
+    deleteButtonsEl.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const currMealEl = e.target.closest('.meal');
+
+        })
+    })
+}
+
+function addMeal() {
+    checkInputFields();
+
+    const headers = {
+        method: 'POST',
+        body: JSON.stringify({
+            food: foodInputEl.value,
+            calories: caloriesInputEl.value,
+            time: timeInputEl.value,
+        }),
+    }
+
+    fetch(baseUrl, headers)
+        .then(loadAllMeals);
+
+    clearInputFields();
+}
+
+function editMeal() {
+    const data = {
+        food: foodInputEl.value,
+        calories: caloriesInputEl.value,
+        time: timeInputEl.value,
+        _id: mealId,
+    }
+
+    fetch (endpoints.update(data._id), {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    }).then(() => {
+        loadAllMeals();
+        clearInputFields();
+        enableAddBtn();
+        mealId = null;
+    });
+}
+
+function deleteMeal() {
+
+}
+
+
 //---------------------------- Helpers ---------------------------------------------------
+function enableEditBtn() {
+    editMealBtnEl.disabled = false;
+    addMealBtnEl.disabled = true;
+}
+
+function enableAddBtn() {
+    addMealBtnEl.disabled = false;
+    editMealBtnEl.disabled = true;
+}
+
+function getIdByName(name) {
+    return fetch(baseUrl)
+        .then(res => res.json())
+        .then(res => Object.entries(res).find(m => m[1].food === name)[1]._id);
+}
+
+async function populateInputFields(f, t, c) {
+    mealId = await getIdByName(f);
+
+    foodInputEl.value = f;
+    timeInputEl.value = t;
+    caloriesInputEl.value = c;
+}
+
+function clearInputFields() {
+    foodInputEl.value = '';
+    timeInputEl.value = '';
+    caloriesInputEl.value = '';
+}
+
 function clearMealList() {
     mealListEl.innerHTML = '';
 }
+
+function checkInputFields() {
+    if (foodInputEl.value === '' || timeInputEl.value === '' || caloriesInputEl.value === '') {
+        return
+    }
+}
+
 attachEvents();
