4ea92bacf2175cca0a77e752aa75fa50c2f72f2f
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-12 11:03:31 +0100

    Initial Commit

--- Exam Preparation/Exam - 5 Aug 2023/02.Scholarship/02.Scholarship_Resources/app.js
+++ Exam Preparation/Exam - 5 Aug 2023/02.Scholarship/02.Scholarship_Resources/app.js
@@ -20,12 +20,55 @@ function solve() {
         return element;
     }
 
-    const field = document.querySelectorAll('#student, #university, #score');
+    const fields = [...document.querySelectorAll('#student, #university, #score')];
     const btnNext = document.querySelector('#next-btn');
 
     const previewListEl = document.querySelector('#preview-list');
     const candidatesListEl = document.querySelector('#candidates-list');
 
+    function createEntry({student, uni, score}) {
+        const entryEl = createElement('li', {
+                className: 'application',
+                dataset: {student, uni, score}
+            },
+            previewListEl);
+        const articleEl = createElement('article', {}, entryEl);
 
+        createElement('p', {textContent: student}, articleEl);
+        createElement('p', {textContent: uni}, articleEl);
+        createElement('p', {textContent: score}, articleEl);
+
+        createElement('button', {
+            className: 'action-btn edit',
+            textContent: 'edit',
+            onclick: editHandler
+        }, entryEl);
+        createElement('button', {
+            className: 'action-btn apply',
+            textContent: 'apply',
+            onclick: applyHandler
+        }, entryEl);
+    }
+
+    btnNext.addEventListener('click', (e) => {
+
+        const [student, uni, score] = fields.map(field => field.value);
+
+        if (!student || !uni || !score) return;
+
+        createEntry({student, uni, score});
+
+        e.target.disabled = true;
+
+        fields.forEach(field => field.value = '');
+    });
+
+    function editHandler(e) {
+
+    }
+
+    function applyHandler(e) {
+
+    }
 }
   
