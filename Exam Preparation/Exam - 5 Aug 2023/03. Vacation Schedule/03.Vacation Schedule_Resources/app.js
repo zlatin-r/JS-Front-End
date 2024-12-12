cb01e5c56c39f793ca98d7f252de1fc17f40b401
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-12 13:04:27 +0100

    Initial Commit

--- Exam Preparation/Exam - 5 Aug 2023/03. Vacation Schedule/03.Vacation Schedule_Resources/app.js
+++ Exam Preparation/Exam - 5 Aug 2023/03. Vacation Schedule/03.Vacation Schedule_Resources/app.js
@@ -1,1 +1,35 @@
-//TODO..
+function loadRecords(baseUrl, onSuccess) {
+    fetch(baseUrl)
+        .then(res => res.json())
+        .then(onSuccess)
+        .catch(error => console.error(error));
+}
+
+function createRecord(baseUrl, record, onSuccess) {
+    fetch(baseUrl, {
+        method: 'POST',
+        body: JSON.stringify(record),
+    })
+        .then(res => res.json())
+        .then(onSuccess)
+        .catch(error => console.error(error));
+}
+
+function updateRecord(baseUrl, record, onSuccess) {
+    fetch(baseUrl + record._id, {
+        method: 'PUT',
+        body: JSON.stringify(record),
+    })
+        .then(res => res.json())
+        .then(onSuccess)
+        .catch(error => console.error(error));
+}
+
+function deleteRecord(baseUrl, record, onSuccess) {
+    fetch(baseUrl + record._id, {
+        method: 'DELETE',
+    })
+        .then(res => res.json())
+        .then(onSuccess)
+        .catch(error => console.error(error));
+}
