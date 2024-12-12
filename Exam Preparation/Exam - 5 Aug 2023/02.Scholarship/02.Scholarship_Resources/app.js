ad4a3b831e39878812132eea12ef48b3e6159272
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-12 10:33:43 +0100

    Initial Commit

--- Exam Preparation/Exam - 5 Aug 2023/02.Scholarship/02.Scholarship_Resources/app.js
+++ Exam Preparation/Exam - 5 Aug 2023/02.Scholarship/02.Scholarship_Resources/app.js
@@ -1,7 +1,26 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    
-  
-  }
+
+    function createElement(tag, properties, container) {
+        const element = document.createElement(tag);
+
+        Object.keys(properties).forEach(key => {
+            if (typeof properties[key] === "object") {
+                Object.assign(element[key], properties[key]);
+            } else {
+                element[key] = properties[key];
+            }
+        });
+
+        if (container) {
+            container.appendChild(element);
+        }
+
+        return element;
+    }
+
+    const field = document.querySelectorAll('#student, #university, #score');
+
+}
   
