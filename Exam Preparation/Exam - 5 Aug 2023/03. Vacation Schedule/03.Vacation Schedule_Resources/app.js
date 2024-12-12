a6fa97110c1002658c42aa1e2c81e1c437cda913
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-12 13:28:33 +0100

    Initial Commit

--- Exam Preparation/Exam - 5 Aug 2023/03. Vacation Schedule/03.Vacation Schedule_Resources/app.js
+++ Exam Preparation/Exam - 5 Aug 2023/03. Vacation Schedule/03.Vacation Schedule_Resources/app.js
@@ -33,3 +33,54 @@ function deleteRecord(baseUrl, record, onSuccess) {
         .then(onSuccess)
         .catch(error => console.error(error));
 }
+
+function createElement(tag, properties, container) {
+    const element = document.createElement(tag);
+
+    Object.keys(properties).forEach(key => {
+        if (typeof properties[key] === "object") {
+            // if (!element[key]) element[key] = {}; // same like: element[key] ??= {};
+            Object.assign(element[key], properties[key]);
+        } else {
+            element[key] = properties[key];
+        }
+    });
+
+    if (container) {
+        container.appendChild(element);
+    }
+
+    return element;
+}
+
+function init() {
+    const baseUrl = 'http://localhost:3030/jsonstore/tasks/'
+
+    const fields = document.querySelectorAll('#form form input');
+    const listEl = document.querySelector('#list');
+
+    function loadEntries() {
+        listEl.innerHTML = '';
+
+        loadRecords(baseUrl, (result) => {
+            Object.values(result).forEach(createEntry)
+        });
+    }
+
+    function createEntry({name, days, date, _id}) {
+        const entryEl = createElement('li',
+            {
+                className: 'container',
+                dataset: {name, days, date}
+            }, listEl);
+        createElement('h2', {textContent: name}, entryEl);
+        createElement('h2', {textContent: days}, entryEl);
+        createElement('h2', {textContent: date}, entryEl);
+        createElement('button', {className: 'change-btn', textContent: 'Change'}, entryEl);
+        createElement('button', {className: 'done-btn', textContent: 'Done'}, entryEl);
+    }
+
+    loadEntries()
+}
+
+init();
