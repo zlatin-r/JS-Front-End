1759da4d68b1fb7c06a1829c3cbfadcb652bcd4b
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-13 13:02:02 +0100

    Initial Commit

---
+++ Exam Preparation/Exam - 8 Apr 2023/03. Sprint Board/03.Sprint-Board_Ресурси/solve.js
@@ -0,0 +1,11 @@
+const baseUrl = 'http://localhost:3030/jsonstore/tasks'
+const endpoints = {
+    update: (id) => `${baseUrl}/${id}`,
+    delete: (id) => `${baseUrl}/${id}`,
+}
+
+function attachEvents() {
+
+}
+
+attachEvents();
