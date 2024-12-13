a1867fa59efb6618f7f97e1c51f0b10aa818a739
Zlatin Rusev <zlatinrusev@gmail.com>
2024-12-13 17:39:59 +0100

    Solution

--- Exam Preparation/Exam - 22 August 2024/03.Car Maintenance Booking/03.Car-Maintenance-Booking_Resources (Copy)/app.js
+++ Exam Preparation/Exam - 22 August 2024/03.Car Maintenance Booking/03.Car-Maintenance-Booking_Resources (Copy)/app.js
@@ -12,11 +12,46 @@ const appDateInputEl = document.querySelector('#date');
 
 const loadBtnEl = document.querySelector('#load-appointments');
 const addAppointmentBtnEl = document.querySelector('#add-appointment');
+const editAppointmentBtnEl = document.querySelector('#edit-appointment');
 
+let appointmentId = null;
 
 function init() {
     loadBtnEl.addEventListener('click', loadAppointments)
     addAppointmentBtnEl.addEventListener('click', addAppointment);
+    editAppointmentBtnEl.addEventListener('click', editAppointment);
+}
+
+async function editAppointment() {
+    const headers = {
+        method: 'PUT',
+        body: JSON.stringify({
+            model: modelInputEl.value,
+            service: serviceInputEl.value,
+            date: appDateInputEl.value,
+            _id: appointmentId,
+        }),
+    }
+
+    fetch(endpoints.update(appointmentId), headers)
+        .then(() => {
+            loadAppointments();
+            clearInputFields();
+            enableAddBtn();
+            appointmentId = null;
+        })
+}
+
+async function deleteAppointment(model) {
+    getIdByModel(model)
+        .then((id) =>
+            fetch(endpoints.delete(id), {
+                method: 'DELETE',
+            }))
+        .then(() => {
+            loadAppointments();
+            appointmentId = null;
+        })
 }
 
 async function addAppointment() {
@@ -77,8 +112,59 @@ async function loadAppointments() {
         newAppointment.appendChild(buttonContainerEl);
 
         appointmentsListEl.appendChild(newAppointment);
-
     });
+    attachEventListeners();
+}
+
+async function attachEventListeners() {
+    const changeButtonsEl = document.querySelectorAll('.change-btn');
+    const deleteButtonEl = document.querySelectorAll('.delete-btn');
+
+    changeButtonsEl.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const currAppointment = e.target.parentElement.parentElement;
+            const carModel = currAppointment.querySelector('h2').textContent;
+            const appDate = currAppointment.querySelector('h3:nth-of-type(1)').textContent;
+            const service = currAppointment.querySelector('h3:nth-of-type(2)').textContent;
+            populateData(carModel, appDate, service);
+        });
+    });
+
+    deleteButtonEl.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const currAppointment = e.target.parentElement.parentElement;
+            const carModel = currAppointment.querySelector('h2').textContent;
+            deleteAppointment(carModel);
+        })
+    })
+}
+
+function getIdByModel(model) {
+    return fetch(baseUrl)
+        .then(res => res.json())
+        .then(res => Object.entries(res).find(a => a[1].model === model)[1]._id);
+}
+
+async function populateData(m, d, s) {
+    appointmentId = await getIdByModel(m);
+
+    console.log(appointmentId);
+
+    modelInputEl.value = m;
+    appDateInputEl.value = d;
+    serviceInputEl.value = s;
+
+    enableEditBtn();
+}
+
+function enableEditBtn() {
+    addAppointmentBtnEl.disabled = true;
+    editAppointmentBtnEl.disabled = false;
+}
+
+function enableAddBtn() {
+    addAppointmentBtnEl.disabled = false;
+    editAppointmentBtnEl.disabled = true;
 }
 
 function clearAppointmentsList() {
@@ -90,6 +176,5 @@ function clearInputFields() {
     serviceInputEl.value = '';
     appDateInputEl.value = '';
 }
-
 
 init();
